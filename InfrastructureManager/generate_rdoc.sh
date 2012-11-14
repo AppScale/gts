f54bf55b74eab6b3c7c014af4d57758e305732b8
Hiranya Jayathilaka <hiranya911@gmail.com>
2012-11-13 19:49:07 -0800

    Removing the old infra manager

--- InfrastructureManager/generate_rdoc.sh
+++
@@ -1,2 +0,0 @@
-rm -rf doc
-rdoc infrastructure_manager.rb infrastructure_manager_server.rb lib
--- PythonInfrastructureManager/agents/euca_agent.py
+++
@@ -1,9 +0,0 @@
-from agents.ec2_agent import EC2Agent
-
-__author__ = 'hiranya'
-
-class EucalyptusAgent(EC2Agent):
-
-    def __init__(self):
-        EC2Agent.__init__(self)
-        self.prefix = 'euca'
