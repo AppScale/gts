59e9356ffbc1050bc9efe9dcff66d4df308685e7
Roberto Aguilar <roberto@appscale.com>
2012-12-07 16:17:56 -0800

    Updated how branch name is discovered.
    Jenkins checks out a hash and the detached head has no way of getting
    the branch information.  So, use `git log --decorate` to detect the
    branch associated with the first commit in the log.

--- bin/build_ec2_image.sh
+++ bin/build_ec2_image.sh
@@ -5,6 +5,6 @@ set -e
 PYTHON=/var/lib/appscale/virtualenvs/appscale-tools/bin/python
 APPSCALE_BOOTSTRAP=/var/lib/appscale/virtualenvs/appscale-tools/bin/appscale-bootstrap
 
-current_branch=$(basename $(git symbolic-ref HEAD))
+current_branch=$(git log --decorate -n 1 | head -n 1 | sed -e 's/.*\(origin\/[^),]*\).*/\1/')
 
 ${PYTHON} ${APPSCALE_BOOTSTRAP} --appscale-branch=${current_branch}
