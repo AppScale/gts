bef716aa86332ee453bf897afa8412c0780a154a
Navraj Chohan <nlake44@gmail.com>
2012-12-21 19:57:35 +0000

    Adding a top level script for building appscale and running unit tests

--- build_script.sh
+++ build_script.sh
@@ -1,26 +1,42 @@
+#!/bin/bash
+echo -n "Checking to make sure you are root..."
+if [ "$(id -u)" != "0" ]; then
+   echo "Failed" 1>&2
+   exit 1
+fi
+echo "Success"
+
 set -e
 set -x
 BRANCH=master
 USER=AppScale
 
+echo -n "Setting root password for this image"
 # Set the password for this image
 passwd
+echo "Success"
 
-# Install git and retrieve repos
+echo -n "Getting source code..."
 apt-get install -y git-core
 git clone https://github.com/$USER/appscale.git $BRANCH
 git clone https://github.com/$USER/appscale-tools.git $BRANCH
+echo "Success"
 
-# Build Image and tools
+echo "Building AppScale Image..."
 cd appscale/debian/
 bash appscale_build.sh
+
+echo "Building AppScale Tools..." 
 cd ../../
 cd appscale-tools/debian
 bash appscale_build.sh
 cd ../../
+echo "****************************"
 echo "Image and tools are complete"
+echo "****************************"
 
 # Run unit tests
+echo "Running Unit tests"
 cd appscale
 rake
 sh ts_python.sh
@@ -28,3 +44,4 @@ sh ts_python.sh
 cd ..
 cd appscale-tools
 rake
+echo "Unit tests complete"
