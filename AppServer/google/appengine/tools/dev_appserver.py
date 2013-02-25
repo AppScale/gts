01296bd2fccd7702c3b988b550b307bfd46c84e3
Navraj Chohan <nlake44@gmail.com>
2013-02-25 17:36:18 +0000

    Fix for deferred authentication

--- AppServer/google/appengine/tools/dev_appserver.py
+++ AppServer/google/appengine/tools/dev_appserver.py
@@ -3915,7 +3915,8 @@ def CreateServer(root_path,
                                        static_caching,
                                        default_partition,
                                        persist_logs,
-                                       interactive_console)
+                                       interactive_console,
+                                       secret_hash=secret_hash)
 
 
   if absolute_root_path not in python_path_list:
