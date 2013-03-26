d4e7c6a4558740f6e0bbbfcc0d82dc59b70c0782
Navraj Chohan <nlake44@gmail.com>
2013-03-26 01:06:10 +0100

    additional testing and framework

--- AppDB/groomer.py
+++ AppDB/groomer.py
@@ -1,29 +1,35 @@
 """ This process grooms the datastore cleaning up old state and 
-calculates datastore statistics. 
+calculates datastore statistics. Removed tombstoned items for garbage 
+collection.
 """
 
+import dbconstants
 import threading
 import time
-
 #from google.appengine.ext import db
 #from google.appengine.ext.db import stats
+import appscale_datastore_batch
 
 class DatastoreGroomer(threading.Thread):
   """ Scans the entire database for each application. """
  
   # The amount of seconds between polling to get the groomer lock.
   LOCK_POLL_PERIOD = 3600
- 
-  def __init__(self, zoo_keeper, datastore):
+
+  # The number of entities retrieved in a datastore request.
+  BATCH_SIZE = 100 
+
+  def __init__(self, zoo_keeper, table_name):
     """ Constructor. 
 
     Args:
       zk: ZooKeeper client.
-      datastore: The datastore client.
+      table_name: The table used (cassandra, hypertable, etc).
     """
     threading.Thread.__init__(self)
     self.zoo_keeper = zoo_keeper
-    self.datastore = datastore
+    self.table_name = table_name
+    self.stats = {}
 
   def run(self):
     """ Starts the main loop of the groomer thread. """
@@ -39,8 +45,49 @@ class DatastoreGroomer(threading.Thread):
       True on success, False otherwise.
     """
     return self.zoo_keeper.get_datastore_groomer_lock()
+
+  def get_entity_batch(self, db_access, last_key=""):
+    """ Gets a batch of entites to operate on.
+
+    Args:
+      db_access: A DatastoreFactory object.
+      last_key: The last key to continue from.
+    Returns:
+      A list of entities.
+    """ 
+    return db.range_query(dbconstants.APP_ENTITY_TABLE, 
+      dbconstants.APP_ENTITY_SCHEMA, last_key, "", self.BATCH_SIZE)
+
+  def reset_statistics(self):
+    """ Reinitializes statistics. """
+    self.stats = {}
+
+  def process_entity(self, entity):
+    """ Processes an entity by updating statistics, indexes, and removes 
+        tombstones.
+
+    Args:
+      entity: The entity to operate on. 
+    Returns:
+      True on success, False otherwise.
+    """
+    return True
  
   def run_groomer(self):
-    """ Runs the grooming process.
+    """ Runs the grooming process. Loops on the entire dataset sequentially
+        and updates stats, indexes, and transactions.
+    Returns:
+      True on success, False otherwise.
     """
-    pass
+    last_key = ""
+    self.reset_statistics()
+    while True:
+      db_access = appscale_datastore_batch.DatastoreFactory.getDatastore(
+        self.table_name)
+      entities = self.get_entity_batch(db_access, last_key=last_key)
+      if not entities:
+        break
+      for entity in entities:
+        self.process_entities(entity)
+    return True
+
--- AppDB/test/unit/test_groomer.py
+++ AppDB/test/unit/test_groomer.py
@@ -10,25 +10,50 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../../../AppServer"))
 from google.appengine.ext import db
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))  
+import appscale_datastore_batch
 import groomer
 from zkappscale import zktransaction as zk
 from zkappscale.zktransaction import ZKTransactionException
+
+class FakeDatastore():
+  def __init__(self):
+    pass
+  def range_query(self, table, schema, start, end, size):
+    return []
+
+class FakeEntity():
+  def __init__(self):
+    pass
 
 class TestGroomer(unittest.TestCase):
   """
   A set of test cases for the datastore groomer thread.
   """
   def test_init(self):
-    datastore = flexmock()
     zookeeper = flexmock()
-    dsg = groomer.DatastoreGroomer(zookeeper, datastore) 
+    dsg = groomer.DatastoreGroomer(zookeeper, "hypertable") 
 
   def test_get_groomer_lock(self):
-    datastore = flexmock()
     zookeeper = flexmock()
     zookeeper.should_receive("get_datastore_groomer_lock").and_return(True)
-    dsg = groomer.DatastoreGroomer(zookeeper, datastore)
+    dsg = groomer.DatastoreGroomer(zookeeper, "hypertable")
     self.assertEquals(True, dsg.get_groomer_lock())
 
+  def test_run_groomer(self):
+    zookeeper = flexmock()
+    dsg = groomer.DatastoreGroomer(zookeeper, "cassandra")
+    dsg = flexmock(dsg)
+    dsg.should_receive("get_entity_batch").and_return([])
+    dsg.should_receive("process_entity")
+    ds_factory = flexmock(appscale_datastore_batch.DatastoreFactory)
+    ds_factory.should_receive("getDatastore").and_return(FakeDatastore())
+    self.assertEquals(True, dsg.run_groomer())
+
+  def test_process_entity(self):
+    zookeeper = flexmock()
+    dsg = groomer.DatastoreGroomer(zookeeper, "cassandra")
+    dsg = flexmock(dsg)
+    self.assertEquals(True, dsg.process_entity(FakeEntity()))
+  
 if __name__ == "__main__":
   unittest.main()    
