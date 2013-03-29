37e1df38416f33185a02fe22bfc34cd85c7ca30e
Brian Drawert <brian@appscale.com>
2013-03-29 17:12:40 +0100

    unit tests started

---
+++ AppDashboard/test/test_dashboard.py
@@ -0,0 +1,81 @@
+import sys
+sys.path.append('/root/appscale/AppServer/lib/webapp2')
+sys.path.append('/root/appscale/AppServer/lib/webob_1_1_1')
+sys.path.append('..')
+sys.path.append('/root/appscale/AppServer/lib/jinja2/')
+sys.path.append('/root/appscale-tools/lib/')
+
+import unittest
+import webapp2
+import re
+from flexmock import flexmock
+
+from appcontroller_client import AppControllerClient
+from local_state import LocalState
+import SOAPpy
+
+from google.appengine.ext import testbed
+from google.appengine.api import users
+
+import datastore_stub_util
+from google.appengine.datastore import datastore_stub_util
+
+
+# from the app main.py
+import dashboard
+
+class TestAppDashboard(unittest.TestCase):
+
+  def setUp(self):
+    self.testbed = testbed.Testbed()
+    self.testbed.activate()
+    self.testbed.init_user_stub()
+    self.testbed.init_datastore_v3_stub()
+    self.testbed.init_taskqueue_stub()
+
+    acc = flexmock(AppControllerClient)
+    acc.should_receive('get_uaserver_host').and_return('public1')
+    acc.should_receive('get_stats').and_return(
+        [{'apps':{ 'app1':True, 'app2':False } }]
+      )
+    acc.should_receive('get_role_info').and_return(
+     [{'jobs':['shadow', 'login'], 'public_ip':'1.1.1.1'} ]
+     )
+    acc.should_receive('get_database_information').and_return(
+      {'table':'fake_database', 'replication':1}
+      )
+    acc.should_receive('get_api_status').and_return(
+      {'api1':'running', 'api2':'failed', 'api3':'unknown'}
+      )
+    acc.should_receive('upload_tgz').and_raise(SOAPpy.Errors.HTTPError)
+    acc.should_receive('stop_app').and_return('true')
+   
+    fake_soap = flexmock(name='fake_soap')
+    soap = flexmock(SOAPpy)
+    soap.should_receive('SOAPProxy').and_return(fake_soap)
+    fake_soap.should_receive('get_app_data').and_return(
+      "\n\n ports: 8080\n num_ports:1\n"
+      )
+    fake_soap.should_receive('get_capabilities').and_return(['upload_app'])
+    fake_soap.should_receive('get_user_data').and_return(
+      "is_cloud_admin:true\napplications:app1:app2\npassword:XXXXXX\n"
+      )
+    fake_soap.should_receive('commit_new_user').and_return('true')
+    fake_soap.should_receive('commit_new_token').and_return()
+    fake_soap.should_receive('get_all_users').and_return("a@a.com:b@a.com")
+    fake_soap.should_receive('set_capabilities').and_return('true')
+
+
+    local = flexmock(LocalState)
+    local.should_receive('encrypt_password').and_return('XXXXXX')
+
+
+  def test_landing(self):
+    request = webapp2.Request.blank('/')
+    request.method = 'GET'
+    response = request.get_response(dashboard.app)
+
+    assert response.status_int == 200
+    assert re.match('FILE:templates/landing/index.html',response)
+    
+ 
---
+++ AppDashboard/test/test_suite.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Programmer: Brian Drawert
+
+import sys
+import unittest
+
+from test_dashboard import TestAppDashboard
+
+
+test_cases = [
+  TestAppDashboard,
+  ]
+
+test_case_names = []
+for cls in test_cases:
+  test_case_names.append(str(cls.__name__))
+
+appscale_test_suite = unittest.TestSuite()
+if len(sys.argv) > 1:
+  if sys.argv[1] in test_case_names:
+    print "only running test "+sys.argv[1]
+    run_test_cases = [sys.argv[1]]
+  else:
+    print "ERROR: unknown test "+sys.argv[1]
+    print "Options are: "+", ".join(test_case_names)
+    sys.exit(1)
+else:
+  run_test_cases = test_case_names
+
+for test_class, test_name in zip(test_cases,test_case_names):
+  if test_name in run_test_cases:
+    tests = unittest.TestLoader().loadTestsFromTestCase(test_class)
+    appscale_test_suite.addTests(tests)
+
+all_tests = unittest.TestSuite([appscale_test_suite])
+unittest.TextTestRunner(verbosity=2).run(all_tests)
