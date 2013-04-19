ed0aafb4eeb2457211184624b2acd9d8508ade17
Chris Bunch <shatterednirvana@gmail.com>
2013-04-19 15:37:05 -0700

    removed unnecessary imports and adding in appid on requests

--- AppServer_Java/src/com/google/appengine/api/log/dev/LocalLogService.java
+++ AppServer_Java/src/com/google/appengine/api/log/dev/LocalLogService.java
@@ -6,11 +6,6 @@ import com.google.appengine.tools.development.LocalRpcService;
 import com.google.appengine.tools.development.LocalRpcService.Status;
 import com.google.appengine.tools.development.ServiceProvider;
 import com.google.apphosting.api.logservice.LogServicePb;
-/*import com.google.apphosting.api.logservice.LogServicePb.LogLine;
-import com.google.apphosting.api.logservice.LogServicePb.LogOffset;
-import com.google.apphosting.api.logservice.LogServicePb.LogReadRequest;
-import com.google.apphosting.api.logservice.LogServicePb.LogReadResponse;
-import com.google.apphosting.api.logservice.LogServicePb.RequestLog; */
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
@@ -28,40 +23,7 @@ import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.HashMap;
-import java.net.HttpURLConnection;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.HostnameVerifier;
-import org.apache.http.conn.ssl.X509HostnameVerifier;
-/*
-import com.google.appengine.api.urlfetch.HTTPHeader;
-import com.google.appengine.api.urlfetch.HTTPMethod;
-import com.google.appengine.api.urlfetch.HTTPRequest;
-import com.google.appengine.api.urlfetch.URLFetchService;
-import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
-*/
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import java.security.cert.X509Certificate;
-import java.security.SecureRandom;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import javax.net.ssl.SSLContext;
-import java.security.KeyManagementException;
-
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.SingleClientConnManager;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.entity.StringEntity;
-import java.security.NoSuchAlgorithmException;
-import javax.security.cert.CertificateException;
 import javax.xml.bind.DatatypeConverter;
 
 @ServiceProvider(LocalRpcService.class)
@@ -216,7 +178,7 @@ public class LocalLogService extends AbstractLocalRpcService
     log.setFinished(complete);
   }
 
-  public synchronized void addAppLogLine(String requestId, long time, int level, String message) throws MalformedURLException, IOException, KeyManagementException, NoSuchAlgorithmException
+  public synchronized void addAppLogLine(String requestId, long time, int level, String message)
   {
     if (message == null)
     {
@@ -227,166 +189,50 @@ public class LocalLogService extends AbstractLocalRpcService
     line.setLevel(level);
     line.setLogMessage(message);
 
-    // CGB
+    // Send the log to the Admin Console for later viewing
     HashMap<String, Object> logHash = new HashMap<String, Object>();
-    System.out.println("time is " + time / 1e6);
     logHash.put("timestamp", time / 1e6);
     logHash.put("level", level);
     logHash.put("message", message);
     HashMap<String, Object>[] logList = new HashMap[1];
     logList[0] = logHash;
-    
 
     Gson gson = new Gson();
     HashMap<String, Object> data = new HashMap<String, Object>();
-    data.put("service_name", "appid");
+    data.put("service_name", System.getProperty("APPLICATION_ID"));
     data.put("host", "192.168.10.2");
     data.put("logs", logList);
 
-    /*
-    SSLContext sslContext = SSLContext.getInstance("SSL");
+    InputStream inStream = null;
+    BufferedInputStream buf = null;
+    InputStreamReader inStreamReader = null;
+    BufferedReader bufferedReader = null;
+    Runtime r = Runtime.getRuntime();
+    String result = null;
+    String base64Data = DatatypeConverter.printBase64Binary(gson.toJson(data).getBytes());
+    try
+    {
+        Process p = r.exec("python /root/appscale/AppServer_Java/src/com/google/appengine/api/log/dev/log_sender.py " + base64Data);
+        inStream = p.getInputStream();
+        buf = new BufferedInputStream(inStream);
+        inStreamReader = new InputStreamReader(buf);
+        bufferedReader = new BufferedReader(inStreamReader);
 
-    // set up a TrustManager that trusts everything
-    sslContext.init(null, new TrustManager[] { new X509TrustManager() {
-            public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-            }
+        String outputLine;
+        while ((outputLine = bufferedReader.readLine()) != null)
+        {
+            result = outputLine;
+        }
 
-            public void checkClientTrusted(X509Certificate[] certs,
-                            String authType) {
-            }
-
-            public void checkServerTrusted(X509Certificate[] certs,
-                            String authType) {
-            }
-    } }, new SecureRandom());
-
-    SSLSocketFactory sf = new SSLSocketFactory(sslContext);
-    Scheme httpsScheme = new Scheme("https", 443, sf);
-    SchemeRegistry schemeRegistry = new SchemeRegistry();
-    schemeRegistry.register(httpsScheme);
-
-    // apache HttpClient version >4.2 should use BasicClientConnectionManager
-    ClientConnectionManager cm = new SingleClientConnManager(schemeRegistry);
-    HttpClient client = new DefaultHttpClient(cm);
-    */
-
-    /*
-    HostnameVerifier hostnameVerifier = org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
-
-    DefaultHttpClient client = new DefaultHttpClient();
-
-    SchemeRegistry registry = new SchemeRegistry();
-    SSLSocketFactory socketFactory = SSLSocketFactory.getSocketFactory();
-    socketFactory.setHostnameVerifier((X509HostnameVerifier) hostnameVerifier);
-    registry.register(new Scheme("https", socketFactory, 443));
-    SingleClientConnManager mgr = new SingleClientConnManager(client.getParams(), registry);
-    DefaultHttpClient httpClient = new DefaultHttpClient(mgr, client.getParams());
-
-    // Set verifier     
-    HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier);
-    */
-
-    /*
-DefaultHttpClient base = new DefaultHttpClient();
-SSLContext ctx = SSLContext.getInstance("TLS");
-X509TrustManager tm = new X509TrustManager() {
-
-    public void checkClientTrusted(X509Certificate[] xcs, String string) {
+        if (p.waitFor() != 0)
+        {
+            System.out.println("Executing python script returned unexpected value: " + p.waitFor());
+        }
     }
-
-    public void checkServerTrusted(X509Certificate[] xcs, String string) {
+    catch(Exception e)
+    {
+        System.out.println("Failed to execute REST call to save log: " + e.getMessage());
     }
-
-    public X509Certificate[] getAcceptedIssuers() {
-        return null;
-    }
-};
-ctx.init(null, new TrustManager[]{tm}, null);
-SSLSocketFactory ssf = new SSLSocketFactory(ctx);
-ssf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-ClientConnectionManager ccm = base.getConnectionManager();
-SchemeRegistry sr = ccm.getSchemeRegistry();
-sr.register(new Scheme("https", ssf, 443));
-
-DefaultHttpClient client = new DefaultHttpClient(ccm, base.getParams());
-
-    HttpPost httpPost = new HttpPost("https://192.168.10.2:443/logs/upload");
-    StringEntity entity = new StringEntity(gson.toJson(data), "UTF-8");
-    entity.setContentType("application/json");
-    httpPost.setEntity(entity);
-    //HttpClient client = new DefaultHttpClient();
-    client.execute(httpPost);
-    */
-
-    /*
-    URLConnection connection = new URL("https://192.168.10.2:443/logs/upload").openConnection();
-    connection.setDoOutput(true);  // Triggers POST.
-    connection.setRequestProperty("Content-Type", "application-json");
-    OutputStream output = null;
-    try {
-      output = connection.getOutputStream();
-      output.write(gson.toJson(data).getBytes());
-    } finally {
-      if (output != null) try { output.close(); } catch (IOException logOrIgnore) {}
-    }
-    InputStream response = connection.getInputStream();
-    */
-
-    /*
-    HttpURLConnection connection = (HttpURLConnection) new URL("https://192.168.10.2:443/logs/upload").openConnection();
-    connection.setDoOutput(true);
-    //connection.setRequestMethod("POST");
-    connection.setRequestProperty("Content-Type", "application-json");
-    OutputStream output = connection.getOutputStream();
-    output.write(gson.toJson(data).getBytes());
-    output.close();
-    //OutputStreamWriter post = new OutputStreamWriter(connection.getOutputStream());
-    //post.write(gson.toJson(data));
-    //post.flush();
-    //post.close();
-    //connection.connect();
-    InputStream response = connection.getInputStream();
-    */
-
-    /*URL url = new URL("https://192.168.10.2:443/logs/upload");
-    HTTPRequest request = new HTTPRequest(url, HTTPMethod.POST);
-    request.setHeader(new HTTPHeader("Content-Type", "application-json"));
-    request.setPayload(gson.toJson(data).getBytes());
-    URLFetchService fetcher = URLFetchServiceFactory.getURLFetchService();
-    fetcher.fetch(request);
-    */
-      InputStream inStream = null;
-      BufferedInputStream buf = null;
-      InputStreamReader inStreamReader = null;
-      BufferedReader bufferedReader = null;
-      Runtime r = Runtime.getRuntime();
-      String result = null;
-      String base64Data = DatatypeConverter.printBase64Binary(gson.toJson(data).getBytes());
-      try
-      {
-          System.out.println("trying to exec command: " + "python /root/appscale/AppServer_Java/src/com/google/appengine/api/log/dev/log_sender.py " + base64Data);
-          Process p = r.exec("python /root/appscale/AppServer_Java/src/com/google/appengine/api/log/dev/log_sender.py " + base64Data);
-          inStream = p.getInputStream();
-          buf = new BufferedInputStream(inStream);
-          inStreamReader = new InputStreamReader(buf);
-          bufferedReader = new BufferedReader(inStreamReader);
-
-          String outputLine;
-          while ((outputLine = bufferedReader.readLine()) != null)
-          {
-              result = outputLine;
-          }
-
-          if (p.waitFor() != 0)
-          {
-              System.out.println("Executing python script returned unexpected value: " + p.waitFor());
-          }
-      }
-      catch(Exception e)
-      {
-          System.out.println("Failed to execute REST call to save log: " + e.getMessage());
-      }
 
     LogServicePb.RequestLog log = findLogInLogMapOrAddNewLog(requestId);
     log.addLine(line);
