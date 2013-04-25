9653abc5d5f468871df62ab9c8aae8a4a844a386
Chris Bunch <shatterednirvana@gmail.com>
2013-04-25 15:56:19 -0700

    removed stubbed login ip address in java log sender, and using public ips to avoid having two entries (public and private) for the same machine

--- AppServer_Java/src/com/google/appengine/api/log/dev/log_sender.py
+++ AppServer_Java/src/com/google/appengine/api/log/dev/log_sender.py
@@ -14,11 +14,35 @@ class LogSender():
 
   @classmethod
   def send_logs(cls, base64_logs):
-    payload = base64.b64decode(base64_logs)
-    conn = httplib.HTTPSConnection('192.168.10.2:443')
+    incomplete_payload = base64.b64decode(base64_logs)
+    log = json.loads(incomplete_payload)
+    log['host'] = cls.get_my_public_hostname()
+    complete_payload = json.dumps(log)
+
+    conn = httplib.HTTPSConnection('{0}:443'.format(cls.get_login_host()))
     headers = {'Content-Type' : 'application/json'}
-    conn.request('POST', '/logs/upload', payload, headers)
+    conn.request('POST', '/logs/upload', complete_payload, headers)
     response = conn.getresponse()
+
+
+  @classmethod
+  def get_my_public_hostname(cls):
+    return cls.read_file("/etc/appscale/my_public_ip")
+
+
+  @classmethod
+  def get_login_host(cls):
+    return cls.read_file("/etc/appscale/login_ip")
+
+
+  @classmethod
+  def read_file(cls, filename):
+    with open(filename, 'r') as file_handle:
+      host = file_handle.read()
+      if host[-1] == "\n":
+        return host[:-1]
+      else:
+        return host
 
 
 if __name__ == '__main__':
