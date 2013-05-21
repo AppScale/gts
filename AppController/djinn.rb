c1097e96d7a15c90a8e2deef9727fe8ff8900358
Navraj Chohan <nlake44@gmail.com>
2013-05-21 14:21:15 -0700

    removed unused arg

--- AppController/djinn.rb
+++ AppController/djinn.rb
@@ -3395,7 +3395,7 @@ HOSTS
 
           pid = app_manager.start_app(app, @appengine_port, 
             get_load_balancer_ip(), @nginx_port, app_language, 
-            xmpp_ip, [Djinn.get_nearest_db_ip(false)],
+            xmpp_ip, [Djinn.get_nearest_db_ip()],
             HelperFunctions.get_app_env_vars(app))
 
           if pid == -1
@@ -3794,7 +3794,7 @@ HOSTS
 
     pid = app_manager.start_app(app, @appengine_port, 
             get_load_balancer_ip(), nginx_port, app_language, 
-            xmpp_ip, [Djinn.get_nearest_db_ip(false)],
+            xmpp_ip, [Djinn.get_nearest_db_ip()],
             HelperFunctions.get_app_env_vars(app))
 
     if pid == -1
