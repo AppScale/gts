662b7b3ff853dd021f5490aa3596b1c1d396f0b7
Navraj Chohan <nlake44@gmail.com>
2013-06-06 00:42:28 +0200

    Fixed typo

--- lib/appscale_info.py
+++ lib/appscale_info.py
@@ -79,8 +79,8 @@ def get_zk_locations_string():
   """ Returns the ZooKeeper connection host string. 
 
   Returns:
-    A string of one or more host:port listing separated by commas.listing separated by commas
-    and None if there was an error with the file. 
+    A string of one or more host:port listing separated by commas
+    and returns None if there was an error with the file or JSON. 
   """
   try:
     info = file_io.read(constants.ZK_LOCATIONS_JSON_FILE) 
