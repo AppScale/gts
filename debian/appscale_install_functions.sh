4e7f7366762ac7c30d82def6fa360d2c45676a93
root <root@appscale-image0.(none)>
2013-06-25 13:57:59 -0700

    setting java home and removing prints

--- debian/appscale_install_functions.sh
+++ debian/appscale_install_functions.sh
@@ -278,12 +278,11 @@ postinstallthrift()
 
 installjavajdk()
 {
-    echo AAA
     sudo echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
     sudo add-apt-repository ppa:webupd8team/java
     sudo apt-get update
     sudo apt-get install -y oracle-java7-installer
-    echo BBB
+    export JAVA_HOME=/usr/lib/jvm/java-7-oracle
 }
 
 installappserverjava()
