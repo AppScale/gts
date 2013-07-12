b8b50a0570ce640c639fbf383bdfbeb2d8cbbca2
root <root@appscale-image0.(none)>
2013-07-12 23:56:15 +0200

    fixing groomer issue

--- AppDB/groomer.py
+++ AppDB/groomer.py
@@ -150,8 +150,8 @@ class DatastoreGroomer(threading.Thread):
     Returns:
       A str representing the app prefix (app_id and namespace).
     """
-    tokens = entity_key.split('/')
-    return tokens[0] + '/' + tokens[1]
+    tokens = entity_key.split(dbconstants.KEY_DELIMITER)
+    return tokens[0] + dbconstants.KEY_DELIMITER + tokens[1]
 
   def process_tombstone(self, key, entity, version):
     """ Processes any entities which have been soft deleted. 
