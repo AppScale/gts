ebdf069a2e50ea1ebc28386e8861a3248f39dde3
Chris Bunch <shatterednirvana@gmail.com>
2013-07-12 14:49:33 -0700

    installing wget before we use it, so that we can wget rabbitmq files

--- debian/appscale_build.sh
+++ debian/appscale_build.sh
@@ -35,7 +35,7 @@ fi
 
 echo "Installing Ubuntu ${DIST} building environment."
 
-apt-get -y install curl
+apt-get -y install curl wget
 curl -d "key=appscale" http://heart-beat.appspot.com/sign
 
 export APPSCALE_HOME_RUNTIME=`pwd`
@@ -79,7 +79,7 @@ apt-get install -y cmake
 
 # install package for build
 apt-get install -y autoconf automake libtool gcc g++ pkg-config ant maven2\
- doxygen graphviz rsync tcl-dev python-tk tk8.4-dev ntp cvs wget\
+ doxygen graphviz rsync tcl-dev python-tk tk8.4-dev ntp cvs\
  bzr xterm screen build-essential bison flex byacc unzip bzip2\
  libc6-dev subversion\
  erlang\
