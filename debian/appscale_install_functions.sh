0eaba50f401ad2e3a800bfbcd097e744cfc8735c
Navraj Chohan <nlake44@gmail.com>
2013-09-22 21:58:22 -0700

    Update appscale_install_functions.sh
    -y flag for java install.

--- debian/appscale_install_functions.sh
+++ debian/appscale_install_functions.sh
@@ -223,7 +223,7 @@ installjavajdk()
     echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
     apt-get update
-    apt-get install oracle-java7-installer
+    apt-get install -y oracle-java7-installer
     export JAVA_HOME=/usr/lib/jvm/java-7-oracle
 }
 
