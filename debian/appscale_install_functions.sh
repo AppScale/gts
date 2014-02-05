e6044d30aa977448b9eebaa644413ac7ea10822e
Navraj Chohan <nlake44@gmail.com>
2014-02-05 17:26:22 +0000

    moved nginx file to amazon s3

--- debian/appscale_install_functions.sh
+++ debian/appscale_install_functions.sh
@@ -328,14 +328,11 @@ installnginx()
     NGINX_VERSION=1.5.10
     mkdir -pv ${APPSCALE_HOME}/downloads
     cd ${APPSCALE_HOME}/downloads
-    wget http://nginx.org/download/nginx-1.5.10.tar.gz
+    wget $APPSCALE_PACKAGE_MIRROR/nginx-1.5.10.tar.gz
     tar zxvf nginx-${NGINX_VERSION}.tar.gz
     rm -v nginx-${NGINX_VERSION}.tar.gz
     pushd nginx-${NGINX_VERSION}
-    #wget $APPSCALE_PACKAGE_MIRROR/v0.23rc2.tar.gz
-    #tar zxvf v0.23rc2.tar.gz
     ./configure --with-http_ssl_module --with-http_gzip_static_module
-    #./configure --add-module=./chunkin-nginx-module-0.23rc2/ --with-http_ssl_module --with-http_gzip_static_module
     make
     make install
     popd
