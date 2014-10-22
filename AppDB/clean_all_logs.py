4d803118c79ce4577a0ab293517630821c01ba3e
Navraj Chohan <nlake44@gmail.com>
2014-10-22 17:27:45 -0500

    allow the script to be run from any node

--- AppDB/clean_all_logs.py
+++ AppDB/clean_all_logs.py
@@ -13,9 +13,10 @@ def main():
   """ This main function allows you to run the groomer manually. """
   zk_connection_locations = appscale_info.get_zk_locations_string()
   zookeeper = zk.ZKTransaction(host=zk_connection_locations)
-  datastore_path = "localhost:8888"
   db_info = appscale_info.get_db_info()
   table = db_info[':table']
+  master = appscale_info.get_db_master_ip()
+  datastore_path = "{0}:8888".format(master)
   ds_groomer = groomer.DatastoreGroomer(zookeeper, table, datastore_path)
   try:
     ds_groomer.remove_old_logs(None)
