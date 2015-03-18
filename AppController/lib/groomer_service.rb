789df62ad7675b9b77e225b46eecbfc042817d0d
Navraj Chohan <nlake44@gmail.com>
2015-03-18 22:47:59 +0000

    fixes from code review

--- AppController/lib/groomer_service.rb
+++ AppController/lib/groomer_service.rb
@@ -6,14 +6,14 @@ require 'helperfunctions'
 require 'monit_interface'
 
 
-# Starts and stop the datastore groomer service.
+# Starts and stops the datastore groomer service.
 module GroomerService
 
   # Starts the Groomer Service on this machine. We don't want to monitor
   # it ourselves, so just tell monit to start it and watch it.
   def self.start()
     start_cmd = "/usr/bin/python /root/appscale/AppDB/groomer_service.py"
-    # stop command doesn't work, relies on terminate.rb
+    # The stop command doesn't work and relies on terminate.rb.
     stop_cmd = "/usr/bin/pkill -9 groomer_service"
     MonitInterface.start(:groomer_service, start_cmd, stop_cmd, "9999", {})
   end
