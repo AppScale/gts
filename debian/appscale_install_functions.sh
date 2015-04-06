db42f3f309e6eb6d77062ce58a76f60db92f834e
root <root@appscale-image0.(none)>
2015-04-06 16:59:08 +0000

    Version bumped to 2.3.0

--- debian/appscale_install_functions.sh
+++ debian/appscale_install_functions.sh
@@ -15,7 +15,7 @@ if [ -z "$APPSCALE_PACKAGE_MIRROR" ]; then
     export APPSCALE_PACKAGE_MIRROR=http://s3.amazonaws.com/appscale-build
 fi
 
-export APPSCALE_VERSION=2.2.0
+export APPSCALE_VERSION=2.3.0
 
 pip_wrapper () 
 {
