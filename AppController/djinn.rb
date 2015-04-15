a78478fc71042c6b9cb04435176ef4812f5b5df1
graziano obertelli <graziano@appscale.com>
2015-04-14 17:20:43 -0700

    Do not override existing authorized_keys.

--- AppController/djinn.rb
+++ AppController/djinn.rb
@@ -3590,23 +3590,31 @@ class Djinn
     Djinn.log_info("Copying SSH keys to node at IP address #{ip}")
     ssh_key = dest_node.ssh_key
 
+    # Commands used in the cloud environment to set the root access.
+    options = "-o StrictHostkeyChecking=no -o NumberOfPasswordPrompts=0"
+    set_authorized_keys = "sudo cat ~#{user_name}/.ssh/authorized_keys /root/.ssh/authorized_keys|uniq -s > #{CONFIG_FILE_LOCATION}/ssh_access"
+    enable_root_login = "sudo cp #{CONFIG_FILE_LOCATION}/ssh_access /root/.ssh/authorized_keys"
+    remove_tmp_file = "sudo rm -f #{CONFIG_FILE_LOCATION}/ssh_access"
+
     HelperFunctions.sleep_until_port_is_open(ip, SSH_PORT)
     Kernel.sleep(3)
 
     if ["ec2", "euca"].include?(@options["infrastructure"])
-      options = "-o StrictHostkeyChecking=no -o NumberOfPasswordPrompts=0"
-      enable_root_login = "sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/"
-      Djinn.log_run("ssh -i #{ssh_key} #{options} 2>&1 ubuntu@#{ip} '#{enable_root_login}'")
+      user_name = "ubuntu"
+      Djinn.log_run("ssh -i #{ssh_key} #{options} 2>&1 #{user_name}@#{ip} '#{set_authorized_keys}'")
+      Djinn.log_run("ssh -i #{ssh_key} #{options} 2>&1 #{user_name}@#{ip} '#{enable_root_login}'")
+      Djinn.log_run("ssh -i #{ssh_key} #{options} 2>&1 #{user_name}@#{ip} '#{remove_tmp_file}'")
     elsif @options["infrastructure"] == "gce"
       # Since GCE v1beta15, SSH keys don't immediately get injected to newly
       # spawned VMs. It takes around 30 seconds, so sleep a bit longer to be
       # sure.
+      user_name = "#{@options['gce_user']}"
       Djinn.log_debug("Waiting for SSH keys to get injected to #{ip}.")
       Kernel.sleep(60)
 
-      options = "-o StrictHostkeyChecking=no -o NumberOfPasswordPrompts=0"
-      enable_root_login = "sudo cp /home/#{@options['gce_user']}/.ssh/authorized_keys /root/.ssh/"
-      Djinn.log_run("ssh -i #{ssh_key} #{options} 2>&1 #{@options['gce_user']}@#{ip} '#{enable_root_login}'")
+      Djinn.log_run("ssh -i #{ssh_key} #{options} 2>&1 #{user_name}@#{ip} '#{set_authorized_keys}'")
+      Djinn.log_run("ssh -i #{ssh_key} #{options} 2>&1 #{user_name}@#{ip} '#{enable_root_login}'")
+      Djinn.log_run("ssh -i #{ssh_key} #{options} 2>&1 #{user_name}@#{ip} '#{remove_tmp_file}'")
     end
 
     secret_key_loc = "#{CONFIG_FILE_LOCATION}/secret.key"
