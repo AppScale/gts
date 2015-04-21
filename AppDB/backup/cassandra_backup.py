efcbb5d51755afd9364870afe7b1c3666aba154c
Navraj Chohan <nlake44@gmail.com>
2015-04-21 23:18:05 +0000

    remove deadcode

--- AppDB/backup/cassandra_backup.py
+++ AppDB/backup/cassandra_backup.py
@@ -9,32 +9,12 @@ from yaml import safe_load
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../lib/"))
 import constants
 
-# Location of cassandra yaml configuration file.
-PATH_TO_YAML = '{0}/AppDB/cassandra/cassandra/conf/cassandra.yaml'.\
-  format(constants.APPSCALE_HOME)
-
 # Full path for the nodetool binary.
 NODE_TOOL = '{0}/AppDB/cassandra/cassandra/bin/nodetool'.\
   format(constants.APPSCALE_HOME)
 
 # Location where we place the tar of the nameshot.
 BACKUP_DIR_LOCATION = "/opt/appscale/backups"
-
-def get_cassandra_config():
-  """ Get the cassandra configuration from its YAML file.
-  """
-  cassy_yaml = open(PATH_TO_YAML)
-  return safe_load(cassy_yaml)
-
-def recursive_dir_backup(dir_path, gs_backup_path):
-  """Backup a directory and all of its contents to Google Cloud Storage.
-
-  Args:
-    dir_path: Path to backup.
-    gs_backup_path: The full GCS path to backup to.
-  """
-  #TODO do not use gsutil
-  call(['gsutil', '-m', 'cp', '-R', dir_path, gs_backup_path])
 
 def clear_old_snapshots():
   """ Remove any old snapshots to minimize diskspace usage both locally. """
