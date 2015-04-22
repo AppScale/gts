48e038433449731f793df414377ddbb181e8765b
Navraj Chohan <nlake44@gmail.com>
2015-04-22 00:31:40 +0000

    updated unit tests

--- AppDB/backup/cassandra_backup.py
+++ AppDB/backup/cassandra_backup.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import tarfile
+import socket
 
 from subprocess import call
 from yaml import safe_load 
@@ -16,6 +17,9 @@ NODE_TOOL = '{0}/AppDB/cassandra/cassandra/bin/nodetool'.\
 # Location where we place the tar of the nameshot.
 BACKUP_DIR_LOCATION = "/opt/appscale/backups"
 
+# File location of where the latest backup goes.
+BACKUP_FILE_LOCATION = "{0}/backup.tar.gz".format(BACKUP_DIR_LOCATION)
+
 def clear_old_snapshots():
   """ Remove any old snapshots to minimize diskspace usage both locally. """
   call([NODE_TOOL, 'clearsnapshot'])
@@ -26,13 +30,11 @@ def create_snapshot(name):
   Args:
     name: The name of the snapshot.
   `"""
-  call([NODE_TOOL, 'snapshot', '-t',  name])
+  call([NODE_TOOL, 'snapshot'])
 
-def get_snapshot_file_names(file_filter):
+def get_snapshot_file_names():
   """ Yields all file names which should be tar'ed up.
 
-  Args:
-    file_filter: The string parameter each file should contain.
   Returns:
     A list of files.
   """
@@ -42,25 +44,41 @@ def get_snapshot_file_names(file_filter):
     for file_name in filenames: 
       path = os.path.join("{0}{1}".format(constants.APPSCALE_DATA_DIR, 
         dirnames), file_name)
-      if file_filter in path and 'snapshots' in path:
+      if 'snapshots' in path:
         file_list.append(path)
   return file_list
 
-def tar_snapshot(name):
+def tar_snapshot(file_paths):
   """ Tars all snapshot files for a given snapshot name.
 
   Args:
-    name: The name of the snapshot. 
-  Returns:
-    The tar file location.
+    file_paths: A list of files to tar up.
   """ 
-  pass
+  call(["rm", "-f", BACKUP_FILE_LOCATION])
+  tar = tarfile.open(BACKUP_FILE_LOCATION, "w")
+  for name in file_paths:
+    tar.add(name)
+  tar.close()
 
-def backup_data_dirs(backup_name):
-  """ Backup every Cassandra data directory recursively. 
+def get_backup_path(bucket_name):
+  """ Returns the path to use when uploading to GCS.
+
+  Args:
+    bucket_name: A str, the bucket name.
+
+  Returns:
+    A str, the path to use when storing to GCS.
+  """
+  hostname = socket.gethostname()
+  return "{0}/{1}/cassandra/".format(bucket_name, hostname)
+ 
+def backup_data(backup_name):
+  """ Backup Cassandra snapshot data directories/files.
 
   Args:
     backup_name: A str, the name of the backup.
   """
+  clear_old_snapshots()
+  create_snapshot()
   files = get_snapshot_file_names(backup_name)
   return files
--- AppDB/test/unit/test_cassandra_backup.py
+++ AppDB/test/unit/test_cassandra_backup.py
@@ -3,12 +3,22 @@
 import datetime
 import os
 import sys
+import tarfile
 import time
+import subprocess
 import unittest
 from flexmock import flexmock
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../backup/"))  
 import cassandra_backup
+
+class FakeTar():
+  def __init__(self):
+    self.add_count = 0
+  def add(self, name):
+    self.add_count += 1
+  def close(self):
+    pass
 
 class TestCassandraBackup(unittest.TestCase):
   """
@@ -20,12 +30,22 @@ class TestCassandraBackup(unittest.TestCase):
     os_mock.should_receive('walk').and_yield(('', ['dirname'], 
       ['/snapshots/xxx'])).once()
     self.assertEquals(['/snapshots/xxx'], 
-      cassandra_backup.get_snapshot_file_names("xxx"))
+      cassandra_backup.get_snapshot_file_names())
 
    # Test with no files present that match the filters.
     os_mock.should_receive('walk').and_yield(('', ['dirname'], 
       ['/what/what'])).once()
-    self.assertEquals([], cassandra_backup.get_snapshot_file_names("xxx"))
-    
+    self.assertEquals([], cassandra_backup.get_snapshot_file_names())
+
+  def test_tar_snapshot(self):
+    flexmock(subprocess)
+    subprocess.should_receive('call').\
+      with_args("rm", "-f", cassandra_backup.BACKUP_FILE_LOCATION)
+    flexmock(tarfile)
+    fake_tar = FakeTar()
+    tarfile.should_receive('open').and_return(fake_tar)
+    cassandra_backup.tar_snapshot(['1', '2'])
+    self.assertEquals(fake_tar.add_count, 2)
+
 if __name__ == "__main__":
   unittest.main()    
