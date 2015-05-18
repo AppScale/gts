bfde9298b321dbc8fa18a98c0abc802393daf3e6
menivaitsi <melpomeni.vaitsi@appscale.com>
2015-05-18 12:47:12 -0700

    Putting the right port in monit for backup_recovery_service.

--- AppController/lib/backup_restore_service.rb
+++ AppController/lib/backup_restore_service.rb
@@ -9,13 +9,16 @@ require 'monit_interface'
 # Starts and stops the backup and recovery service.
 module BackupRecoveryService
 
+  BR_PORT = '8423'
+
   # Starts the BR Service on this machine. We don't want to monitor
   # it ourselves, so just tell monit to start it and watch it.
   def self.start()
     start_cmd = "/usr/bin/python /root/appscale/AppDB/backup/backup_recovery_service.py"
     # The stop command doesn't work and relies on terminate.rb.
     stop_cmd = "/usr/bin/pkill -9 backup_recovery_service"
-    MonitInterface.start(:backup_recovery_service, start_cmd, stop_cmd, "9999", {})
+    MonitInterface.start(:backup_recovery_service, start_cmd, stop_cmd, BR_PORT,
+     {})
   end
 
   # Stops the backup/recovery service running on this machine. Since it's
