7bb3132c31d92e42c1c7b19e410367d7f2ba3ae2
root <root@appscale-image0.(none)>
2015-05-29 17:01:07 +0000

    Disabled verify certificate

--- AppController/lib/user_app_client.rb
+++ AppController/lib/user_app_client.rb
@@ -27,6 +27,7 @@ class UserAppClient
     @secret = secret
     
     @conn = SOAP::RPC::Driver.new("https://#{@ip}:#{SERVER_PORT}")
+    @conn.options["protocol.http.ssl_config.verify_mode"] = nil
     @conn.add_method("change_password", "user", "password", "secret")
     @conn.add_method("commit_new_user", "user", "passwd", "utype", "secret")
     @conn.add_method("commit_new_app", "user", "appname", "language", "secret")
