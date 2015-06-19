6f1bfa94cd7dcb2451ff3ce427acd92aeb05d5ce
menivaitsi <melpomeni.vaitsi@appscale.com>
2015-06-19 13:46:09 -0700

    Implementation of ZK Backup & Recovery using kazoo.

--- AppDB/backup/backup_recovery_helper.py
+++ AppDB/backup/backup_recovery_helper.py
@@ -2,6 +2,38 @@
 
 import logging
 import os
+import statvfs
+import sys
+import tarfile
+from os.path import getsize
+
+import backup_recovery_constants
+import backup_exceptions
+from backup_recovery_constants import BACKUP_DIR_LOCATION
+from backup_recovery_constants import BACKUP_ROLLBACK_SUFFIX
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../lib"))
+from constants import APPSCALE_DATA_DIR
+
+def delete_local_backup_file(local_file):
+  """ Removes the local backup file.
+
+  Args:
+    local_file: A str, the path to the backup file to delete.
+  """
+  if not remove(local_file):
+    logging.warning("No local backup file '{0}' to delete. "
+      "Skipping...".format(local_file))
+
+def delete_secondary_backup(base_path):
+  """ Deletes the secondary backup if it exists, upon successful backup.
+
+  Args:
+    base_path: A str, the full path of the backup file without the secondary
+      suffix.
+  """
+  if not remove("{0}{1}".format(base_path, BACKUP_ROLLBACK_SUFFIX)):
+    logging.warning("No secondary backup to remove. Skipping...")
 
 def does_file_exist(path):
   """ Checks if the given file is in the local filesystem.
@@ -12,6 +44,81 @@ def does_file_exist(path):
     True on success, False otherwise.
   """
   return os.path.isfile(path)
+
+def enough_disk_space(service):
+  """ Checks if there's enough available disk space for a new backup.
+
+  Returns:
+    True on success, False otherwise.
+  """
+  available_space = get_available_disk_space()
+  logging.debug("Available space: {0}".format(available_space))
+
+  backup_size = get_backup_size(service)
+  logging.debug("Backup size: {0}".format(backup_size))
+
+  if backup_size > available_space * \
+    backup_recovery_constants.PADDING_PERCENTAGE:
+    logging.warning("Not enough space for a backup.")
+    return False
+  return True
+
+def get_available_disk_space():
+  """ Returns the amount of available disk space under /opt/appscale.
+
+  Returns:
+    An int, the available disk space in bytes.
+  """
+  stat_struct = os.statvfs(os.path.dirname(BACKUP_DIR_LOCATION))
+  return stat_struct[statvfs.F_BAVAIL] * stat_struct[statvfs.F_BSIZE]
+
+def get_backup_size(service):
+  """ Sums up the size of the snapshot files that consist the backup for the
+  given service.
+
+  Args:
+    service: A str, the service for which we'll calculate the backup size.
+  Returns:
+    An int, the total size of the files consisting the backup in bytes.
+  """
+  backup_files = get_snapshot_paths(service)
+  total_size = sum(getsize(file) for file in backup_files)
+  return total_size
+
+def get_snapshot_paths(service):
+  """ Returns a list of file names holding critical data for the given service.
+
+  Args:
+    service: A str, the service for which we're getting the data files.
+    Currently there is support for Cassandra and Zookeeper.
+  Returns:
+    A list of full paths.
+  """
+  if service == 'cassandra':
+    look_for = 'snapshots'
+  else:
+    return []
+
+  file_list = []
+  data_dir = "{0}{1}".format(APPSCALE_DATA_DIR, service)
+  for full_path, _, file in os.walk(data_dir):
+    if look_for in full_path:
+      file_list.append(full_path)
+  logging.debug("List of data paths for '{0}': {1}".format(
+    service, file_list))
+  return file_list
+
+def move_secondary_backup(base_path):
+  """ Moves the secondary backup back in place, if it exists, upon an un
+  successful backup attempt.
+
+  Args:
+    base_path: A str, the final full path of the backup file after this move.
+  """
+  source = "{0}{1}".format(base_path, BACKUP_ROLLBACK_SUFFIX)
+  target = base_path
+  if not rename(source, target):
+    logging.warning("No secondary backup to restore. Skipping...")
 
 def mkdir(path):
   """ Creates a dir with the given path.
@@ -60,3 +167,64 @@ def remove(path):
       format(path))
     return False
   return True
+
+def remove_dir(path):
+  """ Deletes the given directory from the filesystem.
+
+  Args:
+    path: A str, the path of the directory to delete.
+  Returns:
+    True on success, False otherwise.
+  """
+  try:
+    os.rmdir(path)
+  except OSError:
+    logging.error("OSError while deleting '{0}'".
+      format(path))
+    return False
+  return True
+
+def tar_backup_files(file_paths, target):
+  """ Tars all snapshot files for a given snapshot name.
+
+  Args:
+    file_paths: A list of files to tar up.
+    target: A str, the full path to the tar file to be created.
+  Returns:
+    The path to the tar file, None otherwise.
+  """
+  backup_file_location = target
+
+  # Rename previous backup, if it exists.
+  if not rename(backup_file_location, "{0}{1}".
+      format(backup_file_location, BACKUP_ROLLBACK_SUFFIX)):
+    logging.warning("'{0}' not found. Skipping file rename...".
+      format(backup_file_location))
+
+  # Tar up the backup files.
+  tar = tarfile.open(backup_file_location, "w:gz")
+  for name in file_paths:
+    tar.add(name)
+  tar.close()
+
+  return backup_file_location
+
+def untar_backup_files(source):
+  """ Restores a previous backup into the Cassandra directory structure
+  from a tar ball.
+
+  Args:
+    source: A str, the path to the backup tar.
+  Raises:
+    BRException: On untar issues.
+  """
+  logging.info("Untarring backup file '{0}'...".format(source))
+  try:
+    tar = tarfile.open(source, "r:gz")
+    tar.extractall(path="/")
+    tar.close()
+  except tarfile.TarError, tar_error:
+    logging.exception(tar_error)
+    raise backup_exceptions.BRException(
+      "Exception while untarring backup file '{0}'.".format(source))
+  logging.info("Done untarring '{0}'.".format(source))
--- AppDB/backup/zookeeper_backup.py
+++ AppDB/backup/zookeeper_backup.py
@@ -1,80 +1,135 @@
 """ Zookeeper data backup. """
 
 import json
+import kazoo.client
 import logging
 import os
-import sys
-import tarfile
-import subprocess
-from subprocess import CalledProcessError
 
 import backup_exceptions
+import backup_recovery_helper
 import gcs_helper
+
+from backup_recovery_constants import ZK_IGNORE_PATHS
+from backup_recovery_constants import ZK_TOP_LEVEL
+from backup_recovery_constants import ZOOKEEPER_BACKUP_FILE_LOCATION
 from backup_recovery_constants import StorageTypes
+from backup_recovery_constants import TMP_ZOOKEEPER_BACKUP
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../lib/"))
-import constants
+from zkappscale import shut_down_zookeeper
+from zkappscale.zktransaction import DEFAULT_HOST as ZK_DEFAULT_HOST
+from zkappscale.zktransaction import PATH_SEPARATOR
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../zkappscale/"))
-import start_zookeeper
-import shut_down_zookeeper
+logging.getLogger().setLevel(logging.INFO)
 
-# Location where we place the tar of the backup.
-BACKUP_DIR_LOCATION = "/opt/appscale/backups"
-
-# File location of where the latest backup goes.
-BACKUP_FILE_LOCATION = "{0}/zookeeper_backup.tar.gz".format(BACKUP_DIR_LOCATION)
-
-# Zookeeper data directories.
-ZOOKEEPER_DATA_SUBDIRS = ["version-2"]
-
-def get_zookeeper_snapshot_file_names():
-  """ Yields all file names which should be tar'ed up.
-
-  Returns:
-    A list of files.
-  """
-  file_list = []
-  data_dir = "{0}{1}".format(constants.APPSCALE_DATA_DIR, "zookeeper")
-  for full_path, _, _ in os.walk(data_dir):
-    if 'opt' in full_path:
-      file_list.append(full_path)
-
-  logging.debug("List of snapshot paths: {0}".format(file_list))
-  return file_list
-
-def tar_backup_files(file_paths):
-  """ Tars all snapshot files for a given snapshot name.
+def dump_zk(filename):
+  """ Dumps Zookeeper application data to a file.
 
   Args:
-    file_paths: A list of files to tar up.
-  Returns:
-    The path to the tar file, None otherwise.
+    filename: A str, the path to the temporary Zookeeper backup file.
   """
+  handle = kazoo.client.KazooClient(hosts=ZK_DEFAULT_HOST)
+  handle.start()
+  with open(filename, "wb") as f:
+    recursive_dump(handle, ZK_TOP_LEVEL, f)
+  handle.stop()
 
-  # Create backups dir if not there.
+def recursive_dump(handle, path, file_handler):
+  """ Recursively dumps the path and the value of the children of the given
+  node.
+
+  Args:
+    handle: A Zookeeper client handler.
+    path: The Zookeeper path to dump to a file.
+    file_handler: A file handler to dump the data to.
+  """
   try:
-    subprocess.call(["mkdir", "-p", BACKUP_DIR_LOCATION])
-  except CalledProcessError as error:
-    logging.error("Error while creating dir '{0}'. Error: {1}".
-      format(BACKUP_DIR_LOCATION, str(error)))
-    return None
+    children = handle.get_children(path)
+    if not any(path.startswith(item) for item in ZK_IGNORE_PATHS):
+      logging.debug("Processing path: {0}".format(path))
+      for child in children:
+        logging.debug("Processing child: {0}".format(child))
+        new_path = '{0}{1}'.format(path, child)
+        if path != ZK_TOP_LEVEL:
+          new_path = PATH_SEPARATOR.join([path, child])
+        recursive_dump(handle, new_path, file_handler)
+      if path != ZK_TOP_LEVEL:
+        value = handle.get(path)[0]
+        file_handler.write('{0}\n'.format(json.dumps({path: value})))
+  except kazoo.exceptions.NoNodeError:
+    logging.debug('Reached the end of the zookeeper path.')
 
-  backup_file_location = BACKUP_FILE_LOCATION
+def flush_zk():
+  """ Deletes Zookeeper data. """
+  handle = kazoo.client.KazooClient(hosts=ZK_DEFAULT_HOST)
+  handle.start()
+  recursive_flush(handle, ZK_TOP_LEVEL)
+  handle.stop()
 
-  # Delete previous backup.
+def recursive_flush(handle, path):
+  """ Recursively deletes the path and the value of the children of the given
+  node.
+
+  Args:
+    handle: A Zookeeper client handler.
+    path: The Zookeeper path to delete.
+  """
   try:
-    subprocess.call(["rm", "-f", backup_file_location])
-  except CalledProcessError as error:
-    logging.error("Error while deleting previous backup '{0}'. Error: {1}".
-      format(backup_file_location, str(error)))
+    children = handle.get_children(path)
+    if not any(path.startswith(item) for item in ZK_IGNORE_PATHS):
+      logging.debug("Processing path: {0}".format(path))
+      for child in children:
+        logging.debug("Processing child: {0}".format(child))
+        new_path = '{0}{1}'.format(path, child)
+        if path != ZK_TOP_LEVEL:
+          new_path = PATH_SEPARATOR.join([path, child])
+        recursive_flush(handle, new_path)
+      try:
+        handle.delete(path)
+      except kazoo.exceptions.BadArgumentsError:
+        logging.warning('BadArgumentsError while deleting path: {0}.'.format(
+          path))
+  except kazoo.exceptions.NoNodeError:
+    logging.debug('Reached the end of the zookeeper path.')
 
-  tar = tarfile.open(backup_file_location, "w:gz")
-  for name in file_paths:
-    tar.add(name)
-  tar.close()
+def restore_zk(filename):
+  """ Restores Zookeeper data from a fixed file in the local FS.
 
-  return backup_file_location
+  Args:
+    filename: A str, the path to the temporary Zookeeper backup file.
+  """
+  handle = kazoo.client.KazooClient(hosts=ZK_DEFAULT_HOST)
+  handle.start()
+  with open(filename, 'rb') as f:
+    for line in f.readlines():
+      pair = json.loads(line)
+      path = pair.keys()[0]
+      value = pair.values()[0]
+      try:
+        handle.create(path, bytes(value), makepath=True)
+        logging.debug("Created '{0}'".format(path))
+      except kazoo.exceptions.NodeExistsError:
+        try:
+          handle.set(path, bytes(value))
+          logging.debug("Updated '{0}'".format(path))
+        except kazoo.exceptions.BadArgumentsError:
+          logging.warning("BadArgumentsError for path '{0}'".format(path))
+      except kazoo.exceptions.NoNodeError:
+        logging.warning("NoNodeError for path '{0}'. Parent nodes are "
+          "missing".format(path))
+      except kazoo.exceptions.ZookeeperError:
+        logging.warning("ZookeeperError for path '{0}'".format(path))
+  handle.stop()
+
+def shutdown_zookeeper():
+  """ Top level function for bringing down Zookeeper.
+
+  Returns:
+    True on success, False otherwise.
+  """
+  logging.info("Shutting down Zookeeper.")
+  if not shut_down_zookeeper.run():
+    return False
+  return True
 
 def backup_data(storage, path=''):
   """ Backup Zookeeper directories/files.
@@ -85,122 +140,44 @@ def backup_data(storage, path=''):
   Returns:
     The path to the backup file on success, None otherwise.
   """
-  logging.info("Starting new zk backup.")
-
-  # TODO: Tar up zookeeper data.
-  files = get_zookeeper_snapshot_file_names()
-  if not files:
-    logging.error("No Zookeeper files were found to tar up. Aborting backup...")
+  if storage not in StorageTypes().get_storage_types():
+    logging.error("Storage '{0}' not supported.")
     return None
 
-  tar_file = tar_backup_files(files)
+  logging.info("Starting new zk backup.")
+  dump_zk(TMP_ZOOKEEPER_BACKUP)
+
+  tar_file = backup_recovery_helper.tar_backup_files([TMP_ZOOKEEPER_BACKUP],
+    ZOOKEEPER_BACKUP_FILE_LOCATION)
   if not tar_file:
-    logging.error('Error while tarring up snapshot files. Aborting backup...')
-    remove_local_backup_file(tar_file)
+    logging.error('Error while tarring up Zookeeper files. Aborting backup...')
+    backup_recovery_helper.remove(TMP_ZOOKEEPER_BACKUP)
+    backup_recovery_helper.delete_local_backup_file(tar_file)
+    backup_recovery_helper.move_secondary_backup(tar_file)
     return None
 
   if storage == StorageTypes.LOCAL_FS:
     logging.info("Done with local zk backup!")
+    backup_recovery_helper.remove(TMP_ZOOKEEPER_BACKUP)
+    backup_recovery_helper.\
+      delete_secondary_backup(ZOOKEEPER_BACKUP_FILE_LOCATION)
     return tar_file
   elif storage == StorageTypes.GCS:
     return_value = path
     # Upload to GCS.
     if not gcs_helper.upload_to_bucket(path, tar_file):
       logging.error("Upload to GCS failed. Aborting backup...")
+      backup_recovery_helper.move_secondary_backup(tar_file)
       return_value = None
     else:
       logging.info("Done with zk backup!")
+      backup_recovery_helper.\
+        delete_secondary_backup(ZOOKEEPER_BACKUP_FILE_LOCATION)
 
-    # Remove local backup file.
-    remove_local_backup_file(tar_file)
+    # Remove local backup files.
+    backup_recovery_helper.remove(TMP_ZOOKEEPER_BACKUP)
+    backup_recovery_helper.delete_local_backup_file(tar_file)
     return return_value
-  else:
-    logging.error("Storage '{0}' not supported.")
-    remove_local_backup_file()
-    return None
-
-def shutdown_zookeeper():
-  """ Top level function for bringing down Zookeeper.
-
-  Returns:
-    True on success, False otherwise.
-  """
-  if not shut_down_zookeeper.run():
-    return False
-  return True
-
-def remove_old_data():
-  """ Removes previous node data from the Zookeeper deployment. """
-  for directory in ZOOKEEPER_DATA_SUBDIRS:
-    data_dir = "{0}{1}/{2}".format(constants.APPSCALE_DATA_DIR, "zookeeper",
-      directory)
-    logging.warn("Removing data from {0}".format(data_dir))
-    try:
-      # TODO
-      logging.info("Done removing data!")
-    except CalledProcessError as error:
-      logging.error("Error while removing old data from zk. Overwriting... "
-        "Error: {0}".format(str(error)))
-
-def untar_backup_files():
-  """ Restores a previous backup into the Cassandra directory structure
-  from a tar ball. 
-
-  Raises:
-    BRException: On untar issues.
-  """
-  logging.info("Untarring Cassandra backup files...")
-  try:
-    tar = tarfile.open(BACKUP_FILE_LOCATION, "r:gz")
-    tar.extractall(path="/")
-    tar.close()
-  except tarfile.TarError, tar_error:
-    logging.exception(tar_error)
-    raise backup_exceptions.BRException(
-      "Exception while untarring Zookeeper backup files.")
-  logging.info("Done untarring Zookeeper backup files.")
-
-def restore_snapshots():
-  """ Restore snapshot into correct directories.
-
-  Returns:
-    True on success, False otherwise.
-  """
-  logging.info("Restoring zk snapshot.")
-  for directory in ZOOKEEPER_DATA_SUBDIRS:
-    data_dir = "{0}{1}/{2}/".format(constants.APPSCALE_DATA_DIR, "zookeeper",
-      directory)
-    logging.debug("Restoring in dir {0}".format(data_dir))
-    for path, _, filenames in os.walk(data_dir):
-      for filename in filenames:
-        logging.debug("Restoring: {0}".format(filename))
-        if not filename:
-          logging.warn("skipping...")
-          continue
-        full_path = "{0}/{1}".format(path, filename)
-        new_full_path = "{0}/../../{1}".format(path, filename)
-        logging.debug("{0} -> {1}".format(full_path, new_full_path))
-        # Move the files up into the data directory.
-        try:
-          subprocess.call(['cp', full_path, new_full_path])
-        except CalledProcessError as error:
-          logging.error("Error while moving Zookeeper snapshot in place. "
-            "Error: {0}".format(str(error)))
-          return False
-  logging.info("Done restoring zk snapshot!")
-  return True
-
-def remove_local_backup_file(local_file=BACKUP_FILE_LOCATION):
-  """ Removes the local backup file.
-
-  Args:
-    local_file: A str, the path to the backup file to delete.
-  """
-  try:
-    subprocess.call(['rm', '-rf', local_file])
-  except CalledProcessError as error:
-    logging.error("Error while removing local backup file '{0}'. Error: {1}".\
-      format(local_file, str(error)))
 
 def restore_data(storage, path=''):
   """ Restores the Zookeeper snapshot.
@@ -209,35 +186,53 @@ def restore_data(storage, path=''):
     storage: A str, one of the StorageTypes class members.
     path: A str, the name of the backup file to restore from.
   """
+  if storage not in StorageTypes().get_storage_types():
+    logging.error("Storage '{0}' not supported.")
+    return False
+
+  logging.info("Starting new zk restore.")
+
   if storage == StorageTypes.GCS:
     # Download backup file and store locally with a fixed name.
-    if not gcs_helper.download_from_bucket(path, BACKUP_FILE_LOCATION):
+    if not gcs_helper.download_from_bucket(path,
+        ZOOKEEPER_BACKUP_FILE_LOCATION):
       logging.error("Download from GCS failed. Aborting recovery...")
       return False
 
   # TODO Make sure there's a snapshot to rollback to if restore fails.
   # Not pressing for fresh deployments.
 
-  if not shut_down_zookeeper.run():
-    logging.error("Unable to shut down Zookeeper. Aborting restore...")
-    return False
+  flush_zk()
 
-  remove_old_data()
+  # if not shut_down_zookeeper.run():
+  #   logging.error("Unable to shut down Zookeeper. Aborting restore...")
+  #   if storage == StorageTypes.GCS:
+  #     backup_recovery_helper.\
+  #       delete_local_backup_file(ZOOKEEPER_BACKUP_FILE_LOCATION)
+  #   return False
+  #
+  # # Start Zookeeper.
+  # logging.info("Starting Zookeeper.")
+  # start_zookeeper.run()
+
   try:
-    untar_backup_files()
+    backup_recovery_helper.untar_backup_files(ZOOKEEPER_BACKUP_FILE_LOCATION)
   except backup_exceptions.BRException as br_exception:
-    logging.exception("Exception while restoring zk snapshots. Need to "
-      "rollback... Exception: {0}".format(str(br_exception)))
-  restore_snapshots()
-
-  # Start Zookeeper.
-  start_zookeeper.run()
+    logging.exception("Error while unpacking backup files. Exception: {0}".
+      format(str(br_exception)))
+    if storage == StorageTypes.GCS:
+      backup_recovery_helper.\
+        delete_local_backup_file(ZOOKEEPER_BACKUP_FILE_LOCATION)
+    return False
+  restore_zk(TMP_ZOOKEEPER_BACKUP)
 
   # Local cleanup.
+  backup_recovery_helper.remove(TMP_ZOOKEEPER_BACKUP)
   if storage == StorageTypes.GCS:
-    remove_local_backup_file()
+    backup_recovery_helper.\
+      delete_local_backup_file(ZOOKEEPER_BACKUP_FILE_LOCATION)
 
-  logging.info("Done with restore.")
+  logging.info("Done with zk restore.")
   return True
 
 if "__main__" == __name__:
