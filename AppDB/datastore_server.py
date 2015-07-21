9e3961ab76dc2366265ec55331f7bc4ec72cf4e1
Chris Donati <chris.donati@appscale.com>
2015-07-21 11:45:01 -0700

    Make invalid refs logging debug-only

--- AppDB/datastore_server.py
+++ AppDB/datastore_server.py
@@ -2542,7 +2542,7 @@ class DatastoreDistributed():
       # references that we need.
       current_limit = invalid_refs + zk.MAX_GROUPS_FOR_XG
 
-      logging.info('{} references invalid. Fetching {} more references.'
+      logging.debug('{} references invalid. Fetching {} more references.'
         .format(invalid_refs, current_limit))
 
       last_startrow = startrow
