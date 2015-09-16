ba36aa595bb10737659ec484a9ab9188e98b5ad8
Chris Donati <chris.donati@appscale.com>
2015-09-16 15:08:44 -0700

    Don't create unused AppDashboard pid file

--- AppController/lib/app_dashboard.rb
+++ AppController/lib/app_dashboard.rb
@@ -77,19 +77,18 @@ module AppDashboard
     Djinn.log_info("Starting #{APP_LANGUAGE} app #{APP_NAME}")
     SERVER_PORTS.each { |port|
       Djinn.log_debug("Starting #{APP_LANGUAGE} app #{APP_NAME} on #{HelperFunctions.local_ip()}:#{port}")
-      pid = -1
-      while pid == -1
+      while true
         begin
           pid = app_manager.start_app(APP_NAME, port, uaserver_ip, APP_LANGUAGE,
             login_ip, [uaserver_ip], {})
+          if pid != -1
+            break
+          end
         rescue FailedNodeException
-          pid = -1
-          Djinn.log_info("Failed to start app #{APP_NAME} on "\
+          Djinn.log_info("Failed to start app #{APP_NAME} on " +
             "#{HelperFunctions.local_ip()}:#{port}. Retrying...")
         end
       end
-      pid_file_name = "/etc/appscale/#{APP_NAME}-#{port}.pid"
-      HelperFunctions.write_file(pid_file_name, pid)
     }
 
     begin
