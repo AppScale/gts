9bb9e032395d68d681deb54122e2e3a83ccfe68f
Chris Donati <chris.donati@appscale.com>
2015-09-16 12:58:58 -0700

    Reduce logging when retrying failed make_call

--- AppController/lib/app_controller_client.rb
+++ AppController/lib/app_controller_client.rb
@@ -122,10 +122,10 @@ class AppControllerClient
         rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH,
           OpenSSL::SSL::SSLError, NotImplementedError, Errno::EPIPE,
           Errno::ECONNRESET, SOAP::EmptyResponseError, Exception => e
-          trace = e.backtrace.join("\n")
-          Djinn.log_warn("[#{callr}] exception in make_call to #{@ip}: #{e.class}\n#{trace}")
           if retry_on_except
             Kernel.sleep(1)
+            Djinn.log_debug("[#{callr}] exception in make_call to"\
+              "#{@ip}:#{SERVER_PORT}. Exception class: #{e.class}. Retrying...")
             retry
           else
             raise FailedNodeException.new('Exception encountered while '\
