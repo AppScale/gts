dc3ebb4d5bacfe0447df149fdad557d2473b6be6
graziano obertelli <graziano@appscale.com>
2015-10-22 14:16:48 -0700

    Use some delimeter for debug message.

--- AppController/djinn.rb
+++ AppController/djinn.rb
@@ -3121,8 +3121,8 @@ class Djinn
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         response = http.post(url.path, JSON.dump(instance_info),
           {'Content-Type'=>'application/json'})
-        Djinn.log_debug("Done sending instance info to AppDashboard: info is: " +
-          "#{instance_info.inspect} response is: #{response.body}")
+        Djinn.log_debug("Done sending instance info to AppDashboard. Info is: " +
+          "#{instance_info.inspect}. Response is: #{response.body}.")
       rescue OpenSSL::SSL::SSLError, NotImplementedError, Errno::EPIPE,
         Errno::ECONNRESET => e
         backtrace = e.backtrace.join("\n")
@@ -3162,8 +3162,8 @@ class Djinn
       request = Net::HTTP::Delete.new(url.path)
       request.body = JSON.dump(instance_info)
       response = http.request(request)
-      Djinn.log_debug("Sent delete_instance to AppDashboard: info is: " +
-        "#{instance_info.inspect} response is: #{response.body}")
+      Djinn.log_debug("Sent delete_instance to AppDashboard. Info is: " +
+        "#{instance_info.inspect}. Response is: #{response.body}.")
     rescue Exception => exception
       # Don't crash the AppController because we weren't able to send over
       # the instance info - just continue on.
