5a523e01b8431f8f04cf01b5fc545facd54134b1
Chris Donati <chris.donati@appscale.com>
2015-11-03 09:39:40 -0800

    Log progress based on time passed

--- AppDB/groomer.py
+++ AppDB/groomer.py
@@ -109,9 +109,8 @@ class DatastoreGroomer(threading.Thread):
   # The ID for the task to clean up old dashboard items.
   CLEAN_DASHBOARD_TASK = 'dashboard'
 
-  # Log every time this number of items (index entries, entities, journal
-  # entries, etc.) are processed.
-  LOG_PROGRESS_FREQUENCY = 1000
+  # Log progress every time this many seconds have passed.
+  LOG_PROGRESS_FREQUENCY = 60 * 5
 
   def __init__(self, zoo_keeper, table_name, ds_path):
     """ Constructor.
@@ -140,7 +139,7 @@ class DatastoreGroomer(threading.Thread):
     self.index_entries_checked = 0
     self.index_entries_delete_failures = 0
     self.index_entries_cleaned = 0
-    self.last_logged_index = 0
+    self.last_logged = time.time()
     self.groomer_state = []
 
   def stop(self):
@@ -243,9 +242,10 @@ class DatastoreGroomer(threading.Thread):
         for entity in entities:
           entity.key.delete()
           counter += 1
-          if counter % self.LOG_PROGRESS_FREQUENCY == 0:
-            logging.info('Removed {} {} entities.'
-              .format(counter, model_type.__class__.__name__))
+        if time.time() > self.last_logged + self.LOG_PROGRESS_FREQUENCY:
+          logging.info('Removed {} {} entities.'
+            .format(counter, model_type.__class__.__name__))
+          self.last_logged = time.time()
         if more:
           last_cursor = next_cursor
           self.update_groomer_state([self.CLEAN_DASHBOARD_TASK,
@@ -570,10 +570,10 @@ class DatastoreGroomer(threading.Thread):
         break
 
       self.index_entries_checked += len(references)
-      if (self.index_entries_checked >=
-        self.last_logged_index + self.LOG_PROGRESS_FREQUENCY):
-        logging.info('Checked {} index entries')
-        self.last_logged_index = self.index_entries_checked
+      if time.time() > self.last_logged + self.LOG_PROGRESS_FREQUENCY:
+        logging.info('Checked {} index entries'
+          .format(self.index_entries_checked))
+        self.last_logged = time.time()
       first_ref = references[0].keys()[0]
       logging.debug('Fetched {} total refs, starting with {}, direction: {}'
         .format(self.index_entries_checked, [first_ref], direction))
@@ -1083,8 +1083,9 @@ class DatastoreGroomer(threading.Thread):
         logging.debug("Removing task name {0}".format(entity.timestamp))
         entity.delete()
         counter += 1
-        if counter % self.LOG_PROGRESS_FREQUENCY == 0:
-          logging.info('Removed {} task entities.'.format(counter))
+      if time.time() > self.last_logged + self.LOG_PROGRESS_FREQUENCY:
+        logging.info('Removed {} task entities.'.format(counter))
+        self.last_logged = self.LOG_PROGRESS_FREQUENCY
       self.update_groomer_state([self.CLEAN_TASKS_TASK, last_cursor])
 
     logging.info("Removed {0} task name entities".format(counter))
@@ -1111,8 +1112,9 @@ class DatastoreGroomer(threading.Thread):
 
         last_key = entities[-1].keys()[0]
         self.entities_checked += len(entities)
-        if self.entities_checked % self.LOG_PROGRESS_FREQUENCY == 0:
+        if time.time() > self.last_logged + self.LOG_PROGRESS_FREQUENCY:
           logging.info('Checked {} entities'.format(self.entities_checked))
+          self.last_logged = time.time()
         self.update_groomer_state([self.CLEAN_ENTITIES_TASK, last_key])
       except datastore_errors.Error, error:
         logging.error("Error getting a batch: {0}".format(error))
@@ -1175,8 +1177,9 @@ class DatastoreGroomer(threading.Thread):
         logging.debug("Removing {0}".format(entity))
         entity.key.delete()
         counter += 1
-        if counter % self.LOG_PROGRESS_FREQUENCY == 0:
-          logging.info('Removed {} log entries.'.format(counter))
+      if time.time() > self.last_logged + self.LOG_PROGRESS_FREQUENCY:
+        logging.info('Removed {} log entries.'.format(counter))
+        self.last_logged = time.time()
       if more:
         last_cursor = next_cursor
         self.update_groomer_state([self.CLEAN_LOGS_TASK,
