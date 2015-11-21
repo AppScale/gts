295d3f9d1cf7ae8a1a98dad44a6b21b79b31e333
Chris Donati <chris.donati@appscale.com>
2015-11-20 18:50:50 -0800

    Fix whitespace

--- AppDB/datastore_server.py
+++ AppDB/datastore_server.py
@@ -3395,7 +3395,7 @@ class DatastoreDistributed():
       if direction == datastore_pb.Query_Order.DESCENDING:
         start_value = equality_value
         end_value = index_value + self._TERM_STRING
-    elif oper  == datastore_pb.Query_Filter.EQUAL:
+    elif oper == datastore_pb.Query_Filter.EQUAL:
       if value == "":
         start_value = index_value
         end_value = index_value + self.MIN_INDEX_VALUE + self._TERM_STRING
