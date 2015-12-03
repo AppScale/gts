c1547c5326df5a0116a5c575799d64c436f68dcc
graziano obertelli <graziano@appscale.com>
2015-12-02 22:20:31 -0800

    Add dependency for ifconfig.

--- debian/appscale_build.sh
+++ debian/appscale_build.sh
@@ -57,8 +57,6 @@ if grep docker /proc/1/cgroup > /dev/null ; then
     IN_DOCKER="yes"
     # Make sure we have default locale.
     locale-gen en_US en_US.UTF-8
-    # sshd doesn't run by default on docker.
-    apt-get install -y  openssh-server
 fi
 
 export APPSCALE_HOME_RUNTIME=`pwd`
