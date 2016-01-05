fcd9c4fc6545c631e5c2ad3fac99f8577a0a2d24
graziano obertelli <graziano@appscale.com>
2016-01-05 12:19:55 -0800

    Remove reference to ~/appscale/.appscale.

--- AppController/terminate.rb
+++ AppController/terminate.rb
@@ -1,5 +1,5 @@
 $:.unshift File.join(File.dirname(__FILE__), "lib")
-APPSCALE_HOME = ENV['APPSCALE_HOME']
+CONFIG_FILE_LOCATION = "/etc/appscale"
 
 module TerminateHelper
 
@@ -9,7 +9,7 @@ module TerminateHelper
   # TODO: Use FileUtils.rm_rf instead of backticks throughout this
   # method.
   def self.erase_appscale_state
-    `rm -f #{APPSCALE_HOME}/.appscale/secret.key`
+    `rm -f #{CONFIG_FILE_LOCATION}/secret.key`
     `rm -f /tmp/uploaded-apps`
     `rm -f ~/.appscale_cookies`
     `rm -f /var/appscale/*.pid`
@@ -17,11 +17,11 @@ module TerminateHelper
     `rm -f /etc/monit/conf.d/appscale*.cfg`
     `rm -f /etc/monit/conf.d/controller-17443.cfg`
     `service monit restart`
-    `rm -f /etc/appscale/port-*.txt`
-    `rm -f /etc/appscale/search_ip`
+    `rm -f #{CONFIG_FILE_LOCATION}/port-*.txt`
+    `rm -f #{CONFIG_FILE_LOCATION}/search_ip`
 
     # TODO: Use the constant in djinn.rb (ZK_LOCATIONS_FILE)
-    `rm -rf /etc/appscale/zookeeper_locations.json`
+    `rm -rf #{CONFIG_FILE_LOCATION}/zookeeper_locations.json`
     `rm -f /opt/appscale/appcontroller-state.json`
     `rm -f /opt/appscale/appserver-state.json`
   end
@@ -36,7 +36,7 @@ module TerminateHelper
     # TODO: It may be wise to save the apps for when AppScale starts up
     # later.
     `rm -rf /var/apps/`
-    `rm -rf #{APPSCALE_HOME}/.appscale/*.pid`
+    `rm -rf #{CONFIG_FILE_LOCATION}/*.pid`
     `rm -rf /tmp/ec2/*`
     `rm -rf /tmp/*started`
   end
