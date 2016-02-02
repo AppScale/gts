11feb59abfc0f4ec9f4446eec285d4433dc42a32
lmandres <lmandres@users.noreply.github.com>
2016-02-02 12:27:52 -0500

    Update reinstall_script.sh

--- reinstall_script.sh
+++ reinstall_script.sh
@@ -4,12 +4,15 @@ git clone https://github.com/lmandres/zookeeper-raspberry-pi.git
 git clone https://github.com/lmandres/appscale-raspberry-pi.git
 git clone https://github.com/lmandres/appscale-tools-raspberry-pi.git
 
-sh appscale-raspberry-pi/debian/appscale_build.sh
+mv appscale-raspberry-pi appscale
+mv appscale-tools-raspberry-pi appscale-tools
+
+sh appscale/debian/appscale_build.sh
 cd zookeeper-raspberry-pi
 gem build zookeeper.gemspec
 gem install --local zookeeper-1.4.11.gem
 cd ..
-cp -v ${APPSCALE_HOME}/AppDB/cassandra/templates/cassandra-env.sh ${APPSCALE_HOME}/AppDB/cassandra/cassandra/conf
-sh appscale-tools-raspberry-pi/debian/appscale_build.sh
+cp -v /root/appscale/AppDB/cassandra/templates/cassandra-env.sh /root/appscale/AppDB/cassandra/cassandra/conf
+sh appscale-tools/debian/appscale_build.sh
 
 echo Done!
