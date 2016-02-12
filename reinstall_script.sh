32d7f1a5891a6ac016c64fb722542dc8e3c9da30
lmandres <lmandres@users.noreply.github.com>
2016-02-11 21:52:15 -0500

    Update reinstall_script.sh
    Added -v option to rm and mv.

--- reinstall_script.sh
+++ reinstall_script.sh
@@ -4,10 +4,10 @@ git clone https://github.com/lmandres/zookeeper-raspberry-pi.git
 git clone https://github.com/lmandres/appscale-raspberry-pi.git
 git clone https://github.com/lmandres/appscale-tools-raspberry-pi.git
 
-rm -Rf appscale
-rm -Rf appscale-tools
-mv appscale-raspberry-pi appscale
-mv appscale-tools-raspberry-pi appscale-tools
+rm -Rvf appscale
+rm -Rvf appscale-tools
+mv -v appscale-raspberry-pi appscale
+mv -v appscale-tools-raspberry-pi appscale-tools
 
 sh appscale/debian/appscale_build.sh
 cd zookeeper-raspberry-pi
