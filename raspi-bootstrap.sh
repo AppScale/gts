d6e906c6f00f87e4a6a469b6b405727e3687896f
lmandres <lmandres@users.noreply.github.com>
2016-02-14 01:47:51 -0500

    Update raspi-bootstrap.sh

--- raspi-bootstrap.sh
+++ raspi-bootstrap.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+cd /root
+
 git clone https://github.com/lmandres/appscale-raspberry-pi.git
 git clone https://github.com/lmandres/appscale-tools-raspberry-pi.git
+
+rm -Rf appscale
+rm -Rf appscale-tools
 
 mv -v appscale-raspberry-pi appscale
 mv -v appscale-tools-raspberry-pi appscale-tools
