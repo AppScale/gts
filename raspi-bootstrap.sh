5b1a78b7f5e5c903a44224bef8ea1446dc905cca
lmandres <lmandres@users.noreply.github.com>
2016-02-14 01:27:56 -0500

    Removed extra code
    Previously included code was moved to install scripts.

--- raspi-bootstrap.sh
+++ raspi-bootstrap.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-git clone https://github.com/lmandres/zookeeper-raspberry-pi.git
 git clone https://github.com/lmandres/appscale-raspberry-pi.git
 git clone https://github.com/lmandres/appscale-tools-raspberry-pi.git
 
-rm -Rvf appscale
-rm -Rvf appscale-tools
 mv -v appscale-raspberry-pi appscale
 mv -v appscale-tools-raspberry-pi appscale-tools
 
 sh appscale/debian/appscale_build.sh
-cd zookeeper-raspberry-pi
-gem build zookeeper.gemspec
-gem install --local zookeeper-1.4.11.gem
-cd ..
-cp -v /root/appscale/AppDB/cassandra/templates/cassandra-env.sh /root/appscale/AppDB/cassandra/cassandra/conf
 sh appscale-tools/debian/appscale_build.sh
 
 echo Done!
