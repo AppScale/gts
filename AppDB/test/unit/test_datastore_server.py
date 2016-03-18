81f6849f0000f2c28f46ee3c18e4b8aa957a6bb5
Chris Donati <chris.donati@appscale.com>
2016-03-18 15:29:56 -0700

    Arrange imports alphabetically

--- AppDB/test/unit/test_datastore_server.py
+++ AppDB/test/unit/test_datastore_server.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
 # Programmer: Navraj Chohan <nlake44@gmail.com>
 
+from flexmock import flexmock
 import os
 import sys
 import unittest
-from flexmock import flexmock
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../../AppServer"))  
+from google.appengine.api import api_base_pb
 from google.appengine.datastore import entity_pb
 from google.appengine.datastore import datastore_pb
-from google.appengine.api import api_base_pb
 from google.appengine.ext import db
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))  
 from datastore_server import DatastoreDistributed
 from datastore_server import TOMBSTONE
-from dbconstants import *
-
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))  
-from zkappscale import zktransaction as zk
+from dbconstants import APP_ENTITY_SCHEMA
+from dbconstants import JOURNAL_SCHEMA
+from dbconstants import KEY_DELIMITER
 from zkappscale.zktransaction import ZKTransactionException
 
 class Item(db.Model):
