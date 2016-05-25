4b33607d301df3d8b36aa317b6b53951607a3ab7
Tanvi Marballi <tanvimarballi@gmail.com>
2016-05-25 15:57:22 -0700

    Addressing more code review comments for fetching and processing entities in batches.

--- scripts/datastore_upgrade.py
+++ scripts/datastore_upgrade.py
@@ -15,34 +15,44 @@ import monit_app_configuration
 sys.path.append(os.path.join(os.path.dirname(__file__), '../AppDB'))
 import appscale_datastore_batch
 import datastore_server
+from cassandra_env import cassandra_interface
 from dbconstants import *
 from zkappscale import zktransaction as zk
 
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "../AppServer"))
 from google.appengine.datastore import entity_pb
-
-# The location on the local filesystem where Cassandra is installed.
-CASSANDRA_DIR = "/opt/cassandra"
+from google.appengine.api import datastore_errors
 
 # The location of the Cassandra binary on the local filesystem.
-CASSANDRA_EXECUTABLE = CASSANDRA_DIR + "/cassandra/bin/cassandra"
+CASSANDRA_EXECUTABLE = cassandra_interface.CASSANDRA_INSTALL_DIR \
+  + "/cassandra/bin/cassandra"
 
+# The location on the local file system where we write the process ID
+# that Cassandra runs on.
 PID_FILE = "/var/appscale/appscale-cassandra.pid"
 
 # The default port to connect to Cassandra.
 CASSANDRA_PORT = 9999
 
-# The default port to connect to ZooKeeper.
-ZOOKEEPER_PORT = 2181
-
 # Default ZooKeeper port formatted with a colon.
-ZK_PORT_WITH_COLON = ":" + str(ZOOKEEPER_PORT)
+ZK_PORT_WITH_COLON = ":" + str(zk.DEFAULT_PORT)
 
 # The location on the local filesystem where we should store ZooKeeper data.
 ZK_DATA_LOCATION = "/opt/appscale/zookeeper"
 
-# The maximum number of entities to be returned from the query.
-_MAX_ENTITIES = 1000000
+# The number of entities retrieved in a datastore request.
+BATCH_SIZE = 100
+
+# Log progress every time this many seconds have passed.
+LOG_PROGRESS_FREQUENCY = 30
+
+# Max sleep time for Cassandra and ZooKeeper to be up.
+SLEEP_TIME = 20
+
+CASSANDRA_WATCH_NAME = "cassandra"
+
+ZK_WATCH_NAME = "zookeeper"
 
 def ensure_app_is_not_running():
   """ Ensures AppScale is not running as this is an offline script. """
@@ -55,13 +65,13 @@ def ensure_app_is_not_running():
 def start_cassandra():
   """ Creates a monit configuration file and prompts Monit to start Cassandra. """
   logging.info("Starting Cassandra...")
-  watch = "cassandra"
   start_cmd = CASSANDRA_EXECUTABLE + " start -p " + PID_FILE
   stop_cmd = "/usr/bin/python2 " + APPSCALE_HOME + "/scripts/stop_service.py java cassandra"
-  monit_app_configuration.create_config_file(watch, start_cmd, stop_cmd,
-    ports=[CASSANDRA_PORT], upgrade_flag=True, match_cmd=CASSANDRA_DIR)
-  if not monit_interface.start(watch):
-    logging.info("Unable to start Cassandra.")
+  monit_app_configuration.create_config_file(CASSANDRA_WATCH_NAME, start_cmd, stop_cmd,
+    ports=[CASSANDRA_PORT], upgrade_flag=True,
+    match_cmd=cassandra_interface.CASSANDRA_INSTALL_DIR)
+  if not monit_interface.start(CASSANDRA_WATCH_NAME):
+    logging.error("Unable to start Cassandra.")
     sys.exit(1)
   else:
     logging.info("Successfully started Cassandra.")
@@ -69,200 +79,153 @@ def start_cassandra():
 def start_zookeeper():
   """ Creates a monit configuration file and prompts Monit to start ZooKeeper. """
   logging.info("Starting ZooKeeper...")
-  os.system("rm -rfv /var/lib/zookeeper")
-  os.system("rm -rfv {}".format(ZK_DATA_LOCATION))
+  if os.path.exists("/var/lib/zookeeper"):
+    os.remove("/var/lib/zookeeper")
+
   zk_server = "zookeeper-server"
-  if os.system("service --status-all|grep zookeeper$"):
+  zookeeper_status = subprocess.call(['service', 'zookeeper', 'status']) == 0
+  if zookeeper_status:
     zk_server = "zookeeper"
 
   if not os.path.isdir(ZK_DATA_LOCATION):
     logging.info("Initializing ZooKeeper")
-    os.system("/usr/sbin/service " + "zookeeper" + " stop")
-    os.system("mkdir -pv " + ZK_DATA_LOCATION)
-    os.system("chown -Rv zookeeper:zookeeper " + ZK_DATA_LOCATION)
+    subprocess.call(['/usr/sbin/service', 'zookeeper', 'stop'])
+    os.mkdir(ZK_DATA_LOCATION)
+    subprocess.call(['chown -Rv zookeeper:zookeeper' , ZK_DATA_LOCATION])
 
   if zk_server == "zookeeper-server":
-    if not os.system("/usr/sbin/service " + "zookeeper" + " init"):
-      logging.info("Failed to start zookeeper!")
+    zk_init = subprocess.call(['/usr/sbin/service', 'zookeeper', 'init']) == 0
+    if not zk_init:
+      logging.error("Failed to start zookeeper!")
       sys.exit(1)
 
-  os.system("ln -sfv /etc/zookeeper/conf/myid " + ZK_DATA_LOCATION + "/myid")
-  watch = "zookeeper"
   start_cmd = "/usr/sbin/service " + "zookeeper" + " start"
   stop_cmd = "/usr/sbin/service " + "zookeeper" + " stop"
   match_cmd = "org.apache.zookeeper.server.quorum.QuorumPeerMain"
-  monit_app_configuration.create_config_file(watch, start_cmd, stop_cmd,
-    ports=[ZOOKEEPER_PORT], upgrade_flag=True, match_cmd=match_cmd)
+  monit_app_configuration.create_config_file(ZK_WATCH_NAME, start_cmd, stop_cmd,
+    ports=[zk.DEFAULT_PORT], upgrade_flag=True, match_cmd=match_cmd)
 
-  if not monit_interface.start(watch):
-    logging.info("Unable to start ZooKeeper")
+  if not monit_interface.start(ZK_WATCH_NAME):
+    logging.error("Unable to start ZooKeeper")
     sys.exit(1)
   else:
     logging.info("Successfully started ZooKeeper")
 
 def validate_and_update_entities(datastore, ds_distributed):
-  """ Returns the SOAP server accessor to deal with application and users.
+  """ Validates entities in batches of BATCH_SIZE, deletes tombstoned
+  entities (if any) and updates invalid entities.
   Args:
     datastore: A reference to the batch datastore interface.
+    ds_distributed: A reference to the distributed datastore.
+  """
+  last_key = ""
+  entities_checked = 0
+  last_logged = time.time()
+  while True:
+    try:
+      logging.debug("Fetching {} entities".format(BATCH_SIZE))
+      entities = get_entity_batch(last_key, datastore, BATCH_SIZE)
 
+      if not entities:
+        break
+
+      for entity in entities:
+        process_entity(entity, datastore, ds_distributed)
+
+      last_key = entities[-1].keys()[0]
+      entities_checked += len(entities)
+
+      if time.time() > last_logged + LOG_PROGRESS_FREQUENCY:
+        logging.info("Checked {} entities".format(entities_checked))
+        last_logged = time.time()
+    except datastore_errors.Error as error:
+      logging.error("Error getting a batch of entities: {}".format(error))
+    except AppScaleDBConnectionError as connection_error:
+      logging.error("Error getting a batch of entities: {}".format(connection_error))
+
+def get_entity_batch(last_key, datastore, batch_size):
+  """ Gets a batch of entities to operate on.
+  Args:
+    last_key: The last key from a previous query.
+    datastore: A reference to the batch datastore interface.
+    batch_size: The max number of entities retrieved in the datastore request.
   Returns:
-    A list of current entities in APP_ENTITY_TABLE, entities without tombstoned
-    values and validated entities.
+    A list of entities with a limit of batch_size.
   """
-  # Fetch all entities from the APP_ENTITY_TABLE.
-  try:
-    current_entities = datastore.range_query(APP_ENTITY_TABLE, APP_ENTITY_SCHEMA, "", "", _MAX_ENTITIES)
+  return datastore.range_query(APP_ENTITY_TABLE, APP_ENTITY_SCHEMA, last_key,
+    "", batch_size, start_inclusive=False)
 
-  except Exception as ex:
-    logging.info("Exception on range query: {}".format(str(ex)))
-    sys.exit(1)
-
-  # Remove the tombstoned entities from the list.
-  tombstoneless_entities = ds_distributed.remove_tombstoned_entities(current_entities)
-  app_ids = get_app_ids_from_entities(tombstoneless_entities)
-  entities_to_validate  = tombstoneless_entities
-
-  # Validate the entities grouped by app id.
-  for app_id in app_ids:
-    validated_entities_for_app = ds_distributed.validated_list_result(app_id, entities_to_validate)
-    entities_to_validate = validated_entities_for_app
-
-  return current_entities, tombstoneless_entities, validated_entities_for_app
-
-def delete_tombstoned_entities_from_table(datastore, current_entities, validated_entities):
-  """ Deletes the set of rows corresponding to entity keys which have
-  tombstoned values from the APP_ENTITY_TABLE.
+def process_entity(entity, datastore, ds_distributed):
+  """ Processes an entity by updating it if necessary and removing tombstones.
   Args:
+    entity: The entity to process.
     datastore: A reference to the batch datastore interface.
-    current_entities: A list of entities currently in the APP_ENTITY_TABLE.
-    validated_entities: A list of validated entities to compare from.
+    ds_distributed: A reference to the distributed datastore.
+  Returns:
+    True on success, False otherwise.
   """
-  valid_entity_keys = get_valid_entity_keys(validated_entities)
-  invalid_entity_keys = compare_and_get_invalid_keys(current_entities, valid_entity_keys)
-  try:
-    datastore.batch_delete(APP_ENTITY_TABLE, invalid_entity_keys)
-    logging.info("Batch delete was successful")
-  except AppScaleDBConnectionError as error:
-    logging.info("Exception on batch delete: {}".format(str(error)))
-    sys.exit(1)
+  logging.debug("Process entity {}".format(str(entity)))
+  key = entity.keys()[0]
+  one_entity = entity[key][APP_ENTITY_SCHEMA[0]]
+  version = entity[key][APP_ENTITY_SCHEMA[1]]
+  if one_entity == datastore_server.TOMBSTONE:
+    return delete_entity_from_table(key, datastore)
 
-def update_entities_table(datastore, entities, validated_entities):
-  """ Updates the APP_ENTITY_TABLE with valid entities if any.
+  entity_proto = entity_pb.EntityProto(one_entity)
+  app_id = entity_proto.key().app()
+  validated_entity = ds_distributed.validated_result(app_id, entity)
+
+  if not validated_entity:
+    return True
+
+  if not (one_entity == validated_entity[key][APP_ENTITY_SCHEMA[0]] and version == validated_entity[key][APP_ENTITY_SCHEMA[1]]):
+    return update_entity_in_table(key, validated_entity, datastore)
+  return True
+
+def update_entity_in_table(key, validated_entity, datastore):
+  """ Updates the APP_ENTITY_TABLE with the valid entity.
   Args:
+    key: A str representing the row key to update in the table.
+    validated_entity: A validated entity which needs to be updated in place of
+    the current entity.
     datastore: A reference to the batch datastore interface.
-    entities: A list of tombstonedless entities.
-    validated_entities: A list of validated entities to compare from.
+  Returns:
+    True: if updating entity was successful.
+    False: if an exception was encountered while updating the entity.
   """
-  row_keys_to_update = []
   cell_values_to_update = {}
-  for valid_entity in validated_entities:
-    valid_entity_key = valid_entity.keys()[0]
-    for entity in entities:
-      entity_key = entity.keys()[0]
-      if not valid_entity_key == entity_key:
-        continue
-
-      if is_entity_same(entity,entity_key, valid_entity) and is_txn_id_same(entity, entity_key, valid_entity):
-        continue
-
-      if not entity_key in row_keys_to_update:
-        row_keys_to_update.append(entity_key)
-        cell_value = {}
-        cell_value[APP_ENTITY_SCHEMA[0]] = valid_entity[entity_key][APP_ENTITY_SCHEMA[0]]
-        cell_value[APP_ENTITY_SCHEMA[1]] = valid_entity[entity_key][APP_ENTITY_SCHEMA[1]]
-        cell_values_to_update[entity_key] = cell_value
-
+  cell_values_to_update[key] = {
+    APP_ENTITY_SCHEMA[0]: validated_entity[key][APP_ENTITY_SCHEMA[0]],
+    APP_ENTITY_SCHEMA[1]: validated_entity[key][APP_ENTITY_SCHEMA[1]]
+  }
   try:
-    datastore.batch_put_entity(APP_ENTITY_TABLE, row_keys_to_update, APP_ENTITY_SCHEMA, cell_values_to_update)
-    logging.info("Batch put entities was successful")
-  except  AppScaleDBConnectionError as error:
-    logging.info("Exception on batch put entity: {}".format(str(error)))
-    sys.exit(1)
+    datastore.batch_put_entity(APP_ENTITY_TABLE, [key], APP_ENTITY_SCHEMA, cell_values_to_update)
+  except AppScaleDBConnectionError as connection_error:
+    logging.error("Error updating key {0}: {1}".format(key, connection_error))
+    return False
+  except Exception as ex:
+    logging.exception("Unexpected exception {}".format(ex))
+    return False
+  return True
 
-def is_txn_id_same(entity, key, valid_entity):
-  """ Compares if the transaction id is the same for the current and
-  validated entity.
+def delete_entity_from_table(key, datastore):
+  """ Performs a hard delete on the APP_ENTITY_TABLE for the given row key.
   Args:
-    entity: Current tombstonedless entity.
-    key: A key to look for in the validated entity.
-    valid_entity: A validated entity to compare with.
+    key: A str representing the row key to delete from the table.
+    datastore: A reference to the batch datastore interface.
   Returns:
-    True if transaction id is the same.
+    True: if deleting the entity was successful.
+    False: if an exception was encountered while deleting the entity.
   """
-  return entity[key][APP_ENTITY_SCHEMA[1]] == valid_entity[key][APP_ENTITY_SCHEMA[1]]
-
-def is_entity_same(entity, key, valid_entity):
-  """ Compares if the transaction id is the same for the current and
-  validated entity.
-  Args:
-    entity: Current tombstonedless entity.
-    key: A key to look for in the validated entity.
-    valid_entity: A validated entity to compare with.
-  Returns:
-    True is the encoded entity is the same.
-  """
-  return entity[key][APP_ENTITY_SCHEMA[0]] == valid_entity[key][APP_ENTITY_SCHEMA[0]]
-
-def compare_and_get_invalid_keys(current_entities, valid_entity_keys):
-  """ Looks for the current entities keys in a list of valid entity keys
-  and returns the invalid keys.
-  Args:
-    current_entities: A list of entities currently in APP_ENTITY_TABLE.
-    key: A key to look for in the validated entity.
-    valid_entity_keys: A list valid entity keys to compare from.
-  Returns:
-    A list of invalid entity keys
-  """
-  invalid_entity_keys = []
-  for entity in current_entities:
-    key = entity.keys()[0]
-    if key not in valid_entity_keys:
-      if key not in invalid_entity_keys:
-        invalid_entity_keys.append(key)
-  return invalid_entity_keys
-
-def get_valid_entity_keys(validated_entities):
-  """ Loops through the validated entities and creates a list of valid
-  entity keys.
-  Args:
-    validated_entities: A list of validated entities.
-  Returns:
-    A list of valid entity keys.
-  """
-  valid_row_keys = []
-  # Get all the keys from the validated entities
-  for valid_entity in validated_entities:
-    valid_key = valid_entity.keys()[0]
-    if valid_key not in valid_row_keys:
-      valid_row_keys.append(valid_key)
-  return valid_row_keys
-
-def get_zk_locations_string(zk_location_ips):
-  """ Generates a ZooKeeper IP locations string.
-  Args:
-    zk_location_ips: A list of ZooKeeper IP locations.
-  Returns:
-    A string of ZooKeeper IP locations
-  """
-  return (ZK_PORT_WITH_COLON + ",").join(zk_location_ips) + ZK_PORT_WITH_COLON
-
-def get_app_ids_from_entities(entities_without_tombstoned_values):
-  """ Returns a list of app_ids stripped of the entity keys.
-  Args:
-    entities_without_tombstoned_values: A list of entities which do not have
-    any tombstoned values.
-  Returns:
-    A list of app_ids
-  """
-  app_ids = []
-  for entity in entities_without_tombstoned_values:
-    key = entity.keys()[0]
-    one_entity = entity[key][APP_ENTITY_SCHEMA[0]]
-    entity_proto = entity_pb.EntityProto(one_entity)
-    app_id = entity_proto.key().app()
-    if not app_id in app_ids:
-      app_ids.append(app_id)
-  return app_ids
+  try:
+    datastore.batch_delete(APP_ENTITY_TABLE, [key])
+  except AppScaleDBConnectionError as connection_error:
+    logging.error("Error deleting key {0}: {1}".format(key, connection_error))
+    return False
+  except Exception as ex:
+    logging.exception("Unexpected exception {}".format(ex))
+    return False
+  return True
 
 def get_datastore():
   """ Returns a reference to the batch datastore interface. Validates where
@@ -274,9 +237,7 @@ def get_datastore():
   return datastore_batch
 
 def get_datastore_distributed(datastore, zk_location_ips):
-  """ Returns a reference to the batch datastore interface. Validates where
-  the <datastore>_interface.py is and adds that path to
-  the system path.
+  """ Returns a reference to the distributed datastore.
   Args:
     datastore: A reference to the batch datastore interface.
     zk_location_ips: A list of ZooKeeper location ips.
@@ -284,19 +245,28 @@ def get_datastore_distributed(datastore, zk_location_ips):
   zookeeper_locations = get_zk_locations_string(zk_location_ips)
   zookeeper = zk.ZKTransaction(host=zookeeper_locations)
   datastore_distributed = datastore_server.DatastoreDistributed(datastore, zookeeper=zookeeper)
-  return datastore_distributed
+  return datastore_distributed, zookeeper
+
+def get_zk_locations_string(zk_location_ips):
+  """ Generates a ZooKeeper IP locations string.
+  Args:
+    zk_location_ips: A list of ZooKeeper IP locations.
+  Returns:
+    A string of ZooKeeper IP locations
+  """
+  return (ZK_PORT_WITH_COLON + ",").join(zk_location_ips) + ZK_PORT_WITH_COLON
 
 if __name__ == "__main__":
-  total = len(sys.argv)
-  if not total > 1:
+  args_length = len(sys.argv)
+  if args_length < 2:
     sys.exit(1)
 
   zk_location_ips = []
-  for i in range(total):
+  for index in range(args_length):
     # Skip first argument as that is not a ZooKeeper location.
-    if i == 0:
+    if index == 0:
       continue
-    zk_location_ips.append(str(sys.argv[i]))
+    zk_location_ips.append(str(sys.argv[index]))
 
   # Set up logging.
   level = logging.INFO
@@ -313,18 +283,24 @@ if __name__ == "__main__":
   start_zookeeper()
 
   # Sleep time for Cassandra and ZooKeeper to be started.
-  time.sleep(20)
+  time.sleep(SLEEP_TIME)
 
-  # Loop through entities table and fetch valid entities from journal
-  # table if necessary.
   datastore = get_datastore()
-  ds_distributed = get_datastore_distributed(datastore, zk_location_ips)
-  current_entities, tombstoneless_entities , validated_entities = \
-    validate_and_update_entities(datastore, ds_distributed)
+  ds_distributed, zookeeper = get_datastore_distributed(datastore, zk_location_ips)
 
-  # Delete the tombstoned entities from the APP_ENTITY_TABLE.
-  delete_tombstoned_entities_from_table(datastore, current_entities, validated_entities)
+  # Loop through entities table, fetch valid entities from journal table
+  # if necessary, delete tombstoned entities and updated invalid ones.
+  validate_and_update_entities(datastore, ds_distributed)
 
-  # Update and replace each non valid entity with the right value from the journal.
-  update_entities_table(datastore, tombstoneless_entities, validated_entities)
+  # Close the connection to ZKTransaction.
+  zookeeper.close()
 
+  # Stop Cassandra.
+  if not monit_interface.stop(CASSANDRA_WATCH_NAME):
+    logging.error("Unable to stop Cassandra.")
+    sys.exit(1)
+
+  # Stop ZooKeeper.
+  if not monit_interface.stop(ZK_WATCH_NAME):
+    logging.error("Unable to stop ZooKeeper.")
+    sys.exit(1)
