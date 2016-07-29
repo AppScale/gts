a012f1274bc368e1df96899c4bce9b690d82be78
Chris Donati <chris.donati@appscale.com>
2016-07-28 22:56:43 -0700

    Explicitly define behavior when run as script

--- scripts/read_only.py
+++ scripts/read_only.py
@@ -17,21 +17,22 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "../lib/"))
 import appscale_info
 
-make_active = False
-if len(sys.argv) < 2 or sys.argv[1] not in ['on', 'off']:
-  print('Please give a value of "on" or "off".')
-  exit(1)
+if __name__ == '__main__':
+  make_active = False
+  if len(sys.argv) < 2 or sys.argv[1] not in ['on', 'off']:
+    print('Please give a value of "on" or "off".')
+    sys.exit(1)
 
-if sys.argv[1] == 'on':
-  make_active = True
+  if sys.argv[1] == 'on':
+    make_active = True
 
-acc = appscale_info.get_appcontroller_client()
-result = acc.set_read_only(str(make_active).lower())
-if result != 'OK':
-  print(result)
-  exit(1)
+  acc = appscale_info.get_appcontroller_client()
+  result = acc.set_read_only(str(make_active).lower())
+  if result != 'OK':
+    print(result)
+    sys.exit(1)
 
-if make_active:
-  print('Datastore writes are now disabled.')
-else:
-  print('Datastore writes are now enabled.')
+  if make_active:
+    print('Datastore writes are now disabled.')
+  else:
+    print('Datastore writes are now enabled.')
