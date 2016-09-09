fe66e9ac0368c2298b995bc0c5d4369f5dfe9053
Graziano Obertelli <graziano@appscale.com>
2016-09-08 17:54:45 -0700

    Remove dashboard data only on fresh start.

--- AppController/djinn.rb
+++ AppController/djinn.rb
@@ -1856,9 +1856,19 @@ class Djinn
     # the options for this deployment. It's either a save state from a
     # previous start, or it comes from the tools. If the tools communicate
     # the deployment's data, then we are the headnode.
+    fresh_start = False
     unless restore_appcontroller_state()
+      # Remove old copies of the RESERVED apps code. We need a fresh copy
+      # every time we boot.
+      RESERVED_APPS.each { |reserved_app|
+        app_dir = "#{HelperFunctions.get_app_path(reserved_app)}/app"
+        app_path = "#{PERSISTENT_MOUNT_POINT}/apps/#{reserved_app}.tar.gz"
+        FileUtils.rm_rf(app_dir)
+        FileUtils.rm_rf(app_path)
+      }
       erase_old_data()
       wait_for_data()
+      fresh_start = True
     end
     parse_options()
 
@@ -1878,20 +1888,11 @@ class Djinn
       spawn_and_setup_appengine
     end
 
-    # Remove old copies of the RESERVED apps code. We need a fresh copy
-    # every time we boot.
-    RESERVED_APPS.each { |reserved_app|
-      app_dir = "#{HelperFunctions.get_app_path(reserved_app)}/app"
-      app_path = "#{PERSISTENT_MOUNT_POINT}/apps/#{reserved_app}.tar.gz"
-      FileUtils.rm_rf(app_dir)
-      FileUtils.rm_rf(app_path)
-    }
-
     # Initialize the current server and starts all the API and essential
     # services. The functions are idempotent ie won't restart already
     # running services and can be ran multiple time with no side effect.
-    initialize_server()
-    start_api_services()
+    initialize_server
+    start_api_services(fresh_start)
 
     # Now that we are done loading, we can set the monit job to check the
     # AppController. At this point we are resilient to failure (ie the AC
@@ -3532,7 +3533,7 @@ class Djinn
 
   # Starts all of the services that this node has been assigned to run.
   # Also starts all services that all nodes run in an AppScale deployment.
-  def start_api_services()
+  def start_api_services(fresh_start)
     @state = "Starting API Services."
     Djinn.log_info("#{@state}")
 
@@ -3642,7 +3643,7 @@ class Djinn
 
     # The headnode needs to ensure we have the appscale user, and it needs
     # to prepare the dashboard to be started.
-    if my_node.is_shadow?
+    if my_node.is_shadow? and fresh_start
       threads << Thread.new {
         create_appscale_user()
         prep_app_dashboard()
