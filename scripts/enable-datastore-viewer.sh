2c9fab6be54d35b3dac3c124466caf874a32b5d1
Tanvi Marballi <tanvimarballi@gmail.com>
2016-09-13 13:08:17 -0700

    Addressing code review comments.

--- scripts/enable-datastore-viewer.sh
+++ scripts/enable-datastore-viewer.sh
@@ -7,7 +7,7 @@
 ALLOW=""
 APP_ID=""
 IP="all"
-VIEWER_PORT="8099"
+VIEWER_PORT="8100"
 LOCAL_PORT="30000"
 
 usage() {
@@ -29,18 +29,18 @@ while [ $# -gt 0 ]; do
                 usage
                 exit 1
         fi
-	if [ -n "$1" -a "$1" = "-i" ]; then
-		if [ -n "$2" ]; then
-			IP="$2"
-			ALLOW="allow $IP; 
+        if [ -n "$1" -a "$1" = "-i" ]; then
+                if [ -n "$2" ]; then
+                        IP="$2"
+                        ALLOW="allow $IP;
       deny all;"
-			shift;shift
-			continue
-		else
-			usage
-			exit 1
-		fi
-	fi
+                        shift;shift
+                        continue
+                else
+                        usage
+                        exit 1
+                fi
+        fi
         if [ -n "$1" ]; then
                 APP_ID=$1
                 shift
@@ -53,10 +53,9 @@ if [ ! -e /etc/nginx/sites-enabled ]; then
         echo "ERROR: Cannot find nginx configurations. Is this an AppScale deployment?"
         exit 1
 fi
-
 APPENGINE_IP=""
 for x in $(cat /etc/appscale/all_ips); do
-        OUTPUT=$(ssh $x -i /etc/appscale/keys/cloud1/*.key 'ps ax | grep appserver | grep -Ev '(grep|appscaledashboard)' | grep -- "--admin_port" | sed "s;.*--admin_port \([0-9]*\).*/var/apps/\(.*\)/app .*;\1 \2;g" | sort -ru' | grep "$APP_ID")
+        OUTPUT=$(ssh $x -i /etc/appscale/keys/cloud1/*.key 'ps ax | grep appserver | grep -Ev "(grep|appscaledashboard)" | grep -- "--admin_port" | sed "s;.*--admin_port \([0-9]*\).*/var/apps/\(.*\)/app .*;\1 \2;g" | sort -ru' | grep "$APP_ID")
         for i in $OUTPUT ; do
                 if [ "$i" = "$APP_ID" ]; then
                         continue
@@ -68,8 +67,6 @@ for x in $(cat /etc/appscale/all_ips); do
         done
 done
 
-let $((VIEWER_PORT += 1))
-
 while [[ $(lsof -i :$VIEWER_PORT) ]]; do
         let $((VIEWER_PORT += 1))
 done
@@ -79,7 +76,7 @@ while [[ $(lsof -i :$LOCAL_PORT) ]]; do
 done
 
 # Prepare the nginx config snippet.
-pippo="
+config="
 upstream datastore_viewer_$VIEWER_PORT {
   server localhost:$port;
 }
@@ -99,13 +96,13 @@ server {
 "
 
 if [ -e /etc/nginx/sites-enabled/${APP_ID}.conf ]; then
-		cp /etc/nginx/sites-enabled/${APP_ID}.conf /tmp
-		echo "$pippo" >> /etc/nginx/sites-enabled/${APP_ID}_datastore_viewer.conf
-		echo "Datastore viewer enabled for $APP_ID at http://$(cat /etc/appscale/my_public_ip):$VIEWER_PORT. Allowed IP: $IP."
-		service nginx reload
-		echo "Note: For a multi node deployment, you will need to forward the admin port from one of the AppServers on another node to the head node"
+        cp /etc/nginx/sites-enabled/${APP_ID}.conf /etc/nginx/sites-enabled/${APP_ID}_datastore_viewer.conf
+        echo "$config" >> /etc/nginx/sites-enabled/${APP_ID}_datastore_viewer.conf
+        echo "Datastore viewer enabled for ${APP_ID} at http://$(cat /etc/appscale/my_public_ip):${VIEWER_PORT}. Allowed IP: $IP."
+        service nginx reload
+        echo "Note: For a multi node deployment, you will need to forward the admin port from one of the AppServers on another node to the head node"
         echo "You might need to type in the password for the app engine node."
-        echo "Run the SSH Tunnelling command: ssh -L ${LOCAL_PORT}:localhost:$port $APPENGINE_IP -N"
+        echo "Run the SSH Tunnelling command: ssh -L ${LOCAL_PORT}:localhost:${port} ${APPENGINE_IP} -N"
 else
         echo "Cannot find configuration for ${APP_ID}."
 fi
