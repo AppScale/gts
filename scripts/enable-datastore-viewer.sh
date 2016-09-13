3721899e2197c966440878267382ec1192b09274
Tanvi Marballi <tanvimarballi@gmail.com>
2016-09-13 13:33:12 -0700

    Grep app id in the ssh command.

--- scripts/enable-datastore-viewer.sh
+++ scripts/enable-datastore-viewer.sh
@@ -55,7 +55,7 @@ if [ ! -e /etc/nginx/sites-enabled ]; then
 fi
 APPENGINE_IP=""
 for x in $(cat /etc/appscale/all_ips); do
-        OUTPUT=$(ssh $x -i /etc/appscale/keys/cloud1/*.key 'ps ax | grep appserver | grep -Ev "(grep|appscaledashboard)" | grep -- "--admin_port" | sed "s;.*--admin_port \([0-9]*\).*/var/apps/\(.*\)/app .*;\1 \2;g" | sort -ru' | grep "$APP_ID")
+        OUTPUT=$(ssh $x -i /etc/appscale/keys/cloud1/*.key 'ps ax | grep appserver | grep -E "(grep|$APP_ID)" | grep -- "--admin_port" | sed "s;.*--admin_port \([0-9]*\).*/var/apps/\(.*\)/app .*;\1 \2;g" | sort -ru' | grep "$APP_ID")
         for i in $OUTPUT ; do
                 if [ "$i" = "$APP_ID" ]; then
                         continue
