a846bbad988c16d9115e0a3338b84c7a6d641178
whoarethebritons <eden.akins@gmail.com>
2016-10-13 11:12:13 -0700

    added property and "getter" for dash_layout_settings dash_layout_settings is a TextProperty that is a JSON string describing the layout of navigation and dashboard that a user is allowed to see this string will be returned , loaded from JSON into an OrderedDict in dashboard.py, and used in the templates as a variable

--- AppDashboard/static/js/dash.js
+++ AppDashboard/static/js/dash.js
@@ -2,5 +2,15 @@ $(document).ready(function(){
     //in case there's any later special implementations with multiple areas that can be dragged
     //$( ".selector" ).droppable({accept: ".special"});
 
-    $(".sortable").sortable({cursorAt:{top:3}, cursor:"move", helper:"clone"});
+    $(".sortable").sortable({cursor:"move", helper:"clone",placeholder: "ui-state-highlight"});
+    $(".add-panel").click(function() {
+        $.ajax({
+            method: "post",
+            url:"/ajax/render/panel",
+            data:{page_content:$(this).siblings("a").first().attr("href")+".html"},
+            success: function(result) {
+                $("#dash-panels").append(result);
+            }
+        });
+    });
 });
