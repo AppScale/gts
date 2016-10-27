709820f5ca9e1f02ce42b003dbaf4ccecfe8039b
Chris Donati <chris.donati@appscale.com>
2016-10-26 21:09:07 -0700

    Fix indentation

--- AppServer_Java/src/com/google/appengine/api/blobstore/dev/BlobInfoStorage.java
+++ AppServer_Java/src/com/google/appengine/api/blobstore/dev/BlobInfoStorage.java
@@ -14,93 +14,93 @@ import com.google.appengine.api.datastore.Query;
 import java.util.Iterator;
 
 public final class BlobInfoStorage {
-   private final BlobInfoFactory blobInfoFactory = new BlobInfoFactory();
-   private final DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+    private final BlobInfoFactory blobInfoFactory = new BlobInfoFactory();
+    private final DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
 
-   public BlobInfo loadBlobInfo(BlobKey blobKey) {
-      return this.blobInfoFactory.loadBlobInfo(blobKey);
-   }
+    public BlobInfo loadBlobInfo(BlobKey blobKey) {
+        return this.blobInfoFactory.loadBlobInfo(blobKey);
+    }
 
-   public BlobInfo loadGsFileInfo(BlobKey blobKey) {
-      String namespace = NamespaceManager.get();
+    public BlobInfo loadGsFileInfo(BlobKey blobKey) {
+        String namespace = NamespaceManager.get();
 
-      BlobInfo var5;
-      try {
-         NamespaceManager.set("");
-         Key key = KeyFactory.createKey((Key)null, "__GsFileInfo__", blobKey.getKeyString());
+        BlobInfo var5;
+        try {
+            NamespaceManager.set("");
+            Key key = KeyFactory.createKey((Key)null, "__GsFileInfo__", blobKey.getKeyString());
 
-         try {
-            Entity ex = this.datastoreService.get(key);
-            var5 = (new BlobInfoFactory()).createBlobInfo(ex);
-            return var5;
-         } catch (EntityNotFoundException var9) {
-            var5 = null;
-         }
-      } finally {
-         NamespaceManager.set(namespace);
-      }
+            try {
+                Entity ex = this.datastoreService.get(key);
+                var5 = (new BlobInfoFactory()).createBlobInfo(ex);
+                return var5;
+            } catch (EntityNotFoundException var9) {
+                var5 = null;
+            }
+        } finally {
+            NamespaceManager.set(namespace);
+        }
 
-      return var5;
-   }
+        return var5;
+    }
 
-   public void saveBlobInfo(BlobInfo blobInfo) {
-      String namespace = NamespaceManager.get();
+    public void saveBlobInfo(BlobInfo blobInfo) {
+        String namespace = NamespaceManager.get();
 
-      try {
-         NamespaceManager.set("");
-         Entity entity = new Entity("__BlobInfo__", blobInfo.getBlobKey().getKeyString());
-         entity.setProperty("content_type", blobInfo.getContentType());
-         entity.setProperty("creation", blobInfo.getCreation());
-         entity.setProperty("filename", blobInfo.getFilename());
-         entity.setProperty("size", Long.valueOf(blobInfo.getSize()));
-         entity.setProperty("md5_hash", blobInfo.getMd5Hash());
-         this.datastoreService.put(entity);
-      } finally {
-         NamespaceManager.set(namespace);
-      }
+        try {
+            NamespaceManager.set("");
+            Entity entity = new Entity("__BlobInfo__", blobInfo.getBlobKey().getKeyString());
+            entity.setProperty("content_type", blobInfo.getContentType());
+            entity.setProperty("creation", blobInfo.getCreation());
+            entity.setProperty("filename", blobInfo.getFilename());
+            entity.setProperty("size", Long.valueOf(blobInfo.getSize()));
+            entity.setProperty("md5_hash", blobInfo.getMd5Hash());
+            this.datastoreService.put(entity);
+        } finally {
+            NamespaceManager.set(namespace);
+        }
 
-   }
+    }
 
-   public void deleteBlobInfo(BlobKey blobKey) {
-      this.datastoreService.delete(new Key[]{this.getMetadataKeyForBlobKey(blobKey)});
-   }
+    public void deleteBlobInfo(BlobKey blobKey) {
+        this.datastoreService.delete(new Key[]{this.getMetadataKeyForBlobKey(blobKey)});
+    }
 
-   protected Key getMetadataKeyForBlobKey(BlobKey blobKey) {
-      String namespace = NamespaceManager.get();
+    protected Key getMetadataKeyForBlobKey(BlobKey blobKey) {
+        String namespace = NamespaceManager.get();
 
-      Key var3;
-      try {
-         NamespaceManager.set("");
-         if(blobKey.getKeyString().startsWith("encoded_gs_key:")) {
-            var3 = KeyFactory.createKey("__GsFileInfo__", blobKey.getKeyString());
-            return var3;
-         }
+        Key var3;
+        try {
+            NamespaceManager.set("");
+            if(blobKey.getKeyString().startsWith("encoded_gs_key:")) {
+                var3 = KeyFactory.createKey("__GsFileInfo__", blobKey.getKeyString());
+                return var3;
+            }
 
-         var3 = KeyFactory.createKey("__BlobInfo__", blobKey.getKeyString());
-      } finally {
-         NamespaceManager.set(namespace);
-      }
+            var3 = KeyFactory.createKey("__BlobInfo__", blobKey.getKeyString());
+        } finally {
+            NamespaceManager.set(namespace);
+        }
 
-      return var3;
-   }
+        return var3;
+    }
 
-   void deleteAllBlobInfos() {
-      String namespace = NamespaceManager.get();
+    void deleteAllBlobInfos() {
+        String namespace = NamespaceManager.get();
 
-      Query q;
-      try {
-         NamespaceManager.set("");
-         q = new Query("__BlobInfo__");
-      } finally {
-         NamespaceManager.set(namespace);
-      }
+        Query q;
+        try {
+            NamespaceManager.set("");
+            q = new Query("__BlobInfo__");
+        } finally {
+            NamespaceManager.set(namespace);
+        }
 
-      Iterator i$ = this.datastoreService.prepare(q).asIterable().iterator();
+        Iterator i$ = this.datastoreService.prepare(q).asIterable().iterator();
 
-      while(i$.hasNext()) {
-         Entity e = (Entity)i$.next();
-         this.datastoreService.delete(new Key[]{e.getKey()});
-      }
+        while(i$.hasNext()) {
+            Entity e = (Entity)i$.next();
+            this.datastoreService.delete(new Key[]{e.getKey()});
+        }
 
-   }
+    }
 }
