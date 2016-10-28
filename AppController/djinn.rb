e101aea13cacc5bbb844cb29b8ed67e038442aa2
Graziano Obertelli <graziano@appscale.com>
2016-10-28 16:36:32 -0700

    Don't set useless variables
    Those certificates we used to set for EC2 are actually not used. Also
    tighten the security on the ssh keys.

--- AppController/djinn.rb
+++ AppController/djinn.rb
@@ -3360,17 +3360,14 @@ class Djinn
     Djinn.log_debug("Creating directory #{my_key_dir} for my ssh key #{my_key_loc}")
     FileUtils.mkdir_p(my_key_dir)
     Djinn.log_run("cp #{APPSCALE_CONFIG_DIR}/ssh.key #{my_key_loc}")
+    Djinn.log_run("chmod 600 #{APPSCALE_CONFIG_DIR}/ssh.key")
+    Djinn.log_run("chmod 600 #{my_key_log}")
 
-    if is_cloud?
-      # for euca
+    # AWS and Euca need some evironmental variables.
+    if ["ec2", "euca"].include?(@options['infrastructure'])
       ENV['EC2_ACCESS_KEY'] = @options['ec2_access_key']
       ENV['EC2_SECRET_KEY'] = @options['ec2_secret_key']
       ENV['EC2_URL'] = @options['ec2_url']
-
-      # for ec2
-      cloud_keys_dir = File.expand_path("#{APPSCALE_CONFIG_DIR}/keys/cloud1")
-      ENV['EC2_PRIVATE_KEY'] = "#{cloud_keys_dir}/mykey.pem"
-      ENV['EC2_CERT'] = "#{cloud_keys_dir}/mycert.pem"
     end
 
     write_database_info()
@@ -4035,14 +4032,11 @@ class Djinn
 
     cloud_keys_dir = File.expand_path("#{APPSCALE_CONFIG_DIR}/keys/cloud1")
     make_dir = "mkdir -p #{cloud_keys_dir}"
-
-    cloud_private_key = "#{cloud_keys_dir}/mykey.pem"
-    cloud_cert = "#{cloud_keys_dir}/mycert.pem"
+    change_permissions = "chmod 600 #{APPSCALE_CONFIG_DIR}/ssh.key}"
 
     HelperFunctions.run_remote_command(ip, make_dir, ssh_key, NO_OUTPUT)
-    HelperFunctions.scp_file(ssh_key, ssh_key, ip, ssh_key)
-    HelperFunctions.scp_file(cloud_private_key, cloud_private_key, ip, ssh_key)
-    HelperFunctions.scp_file(cloud_cert, cloud_cert, ip, ssh_key)
+    HelperFunctions.scp_file(ssh_key, "#{APPSCALE_CONFIG_DIR}/ssh.key", ip, ssh_key)
+    HelperFunctions.run_remote_command(ip, change_permissions, ssh_key, NO_OUTPUT)
 
     # Finally, on GCE, we need to copy over the user's credentials, in case
     # nodes need to attach persistent disks.
