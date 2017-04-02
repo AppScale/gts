49e6cca3396350625ae593dcafaf47ec9a2b110d
Chris Donati <chris.donati@appscale.com>
2017-04-02 11:09:43 -0700

    Create appscale-common package
    This can be used by other AppScale packages.

---
+++ common/appscale/common/unpackaged.py
@@ -0,0 +1,8 @@
+""" This module refers to unpackaged directories needed by this package. """
+import os
+
+# The location of the AppScale git repository.
+APPSCALE_HOME = os.path.join('/root', 'appscale')
+
+# The location of the Python AppServer.
+APPSCALE_PYTHON_APPSERVER = os.path.join(APPSCALE_HOME, 'AppServer')
