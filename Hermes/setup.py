669ed00c208980380f20f31f0e6b74f16b380700
Anton Leonov <jeanleonov@gmail.com>
2017-07-05 13:14:48 +0300

    Update Hermes version in setup script

--- Hermes/setup.py
+++ Hermes/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='appscale-hermes',
-  version='0.0.4',
+  version='0.0.5',
   description='AppScale module which takes care of periodical backup and '
               'restore tasks and provides statistics API.',
   author='AppScale Systems, Inc.',
