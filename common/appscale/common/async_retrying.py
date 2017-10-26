377fdb479e85597585a7c57eb95b8287c59eb3d1
Anton Leonov <jeanleonov@gmail.com>
2017-10-26 16:56:25 +0300

    Create new instance of decorator class when receive new parameters

--- common/appscale/common/async_retrying.py
+++ common/appscale/common/async_retrying.py
@@ -8,19 +8,40 @@ import time
 from tornado import locks, gen
 from tornado.ioloop import IOLoop
 
-from appscale.common.retrying import DEFAULT_BACKOFF_BASE, \
-  DEFAULT_BACKOFF_MULTIPLIER, DEFAULT_BACKOFF_THRESHOLD, DEFAULT_MAX_RETRIES, \
-  DEFAULT_RETRYING_TIMEOUT, DEFAULT_RETRY_ON_EXCEPTION, MISSED
+from appscale.common.retrying import (
+  DEFAULT_BACKOFF_BASE, DEFAULT_BACKOFF_MULTIPLIER, DEFAULT_BACKOFF_THRESHOLD,
+  DEFAULT_MAX_RETRIES, DEFAULT_RETRYING_TIMEOUT, DEFAULT_RETRY_ON_EXCEPTION,
+  MISSED, not_missed
+)
 
 
 class _RetryCoroutine(object):
-  def __init__(self):
-    self.backoff_base = DEFAULT_BACKOFF_BASE
-    self.backoff_multiplier = DEFAULT_BACKOFF_MULTIPLIER
-    self.backoff_threshold = DEFAULT_BACKOFF_THRESHOLD
-    self.max_retries = DEFAULT_MAX_RETRIES
-    self.retrying_timeout = DEFAULT_RETRYING_TIMEOUT
-    self.retry_on_exception = DEFAULT_RETRY_ON_EXCEPTION
+  def __init__(self, backoff_base=MISSED, backoff_multiplier=MISSED,
+               backoff_threshold=MISSED, max_retries=MISSED,
+               retrying_timeout=MISSED, retry_on_exception=MISSED):
+    """
+    Args:
+      backoff_base: a number to use in backoff calculation.
+      backoff_multiplier: a number indicating initial backoff.
+      backoff_threshold: a number indicating maximum backoff.
+      max_retries: an integer indicating maximum number of retries.
+      retrying_timeout: a number indicating number of seconds after which
+        retrying should be stopped.
+      retry_on_exception: a function receiving one argument: exception object
+        and returning True if retry makes sense for such exception.
+        Alternatively you can pass list of exception types for which
+        retry is needed.
+    """
+    self.backoff_base = not_missed(backoff_base, DEFAULT_BACKOFF_BASE)
+    self.backoff_multiplier = not_missed(backoff_multiplier,
+                                         DEFAULT_BACKOFF_MULTIPLIER)
+    self.backoff_threshold = not_missed(backoff_threshold,
+                                        DEFAULT_BACKOFF_THRESHOLD)
+    self.max_retries = not_missed(max_retries, DEFAULT_MAX_RETRIES)
+    self.retrying_timeout = not_missed(retrying_timeout,
+                                       DEFAULT_RETRYING_TIMEOUT)
+    self.retry_on_exception = not_missed(retry_on_exception,
+                                         DEFAULT_RETRY_ON_EXCEPTION)
 
   def __call__(self, func=None, backoff_base=MISSED, backoff_multiplier=MISSED,
                backoff_threshold=MISSED, max_retries=MISSED,
@@ -44,21 +65,20 @@ class _RetryCoroutine(object):
     Returns:
       A wrapped coroutine or self if coroutine arg was omitted in arguments.
     """
-    if backoff_base is not MISSED:
-      self.backoff_base = backoff_base
-    if backoff_multiplier is not MISSED:
-      self.backoff_multiplier = backoff_multiplier
-    if backoff_threshold is not MISSED:
-      self.backoff_threshold = backoff_threshold
-    if max_retries is not MISSED:
-      self.max_retries = max_retries
-    if retrying_timeout is not MISSED:
-      self.retrying_timeout = retrying_timeout
-    if retry_on_exception is not MISSED:
-      self.retry_on_exception = retry_on_exception
-
     if func is None:
-      return self
+      return _RetryCoroutine(
+        backoff_base=backoff_base,
+        backoff_multiplier=backoff_multiplier,
+        backoff_threshold=backoff_threshold,
+        max_retries=max_retries,
+        retrying_timeout=retrying_timeout,
+        retry_on_exception=retry_on_exception
+      )
+
+    args = (backoff_base, backoff_multiplier, backoff_threshold, max_retries,
+            retrying_timeout, retry_on_exception)
+    if any((argument is not MISSED) for argument in args):
+      raise TypeError('You should pass either func or parameters')
 
     coroutine = gen.coroutine(func)
 
--- common/appscale/common/retrying.py
+++ common/appscale/common/retrying.py
@@ -14,14 +14,45 @@ DEFAULT_RETRY_ON_EXCEPTION = None   # Retry after any exception.
 MISSED = object()
 
 
+class BothMissedException(Exception):
+  pass
+
+
+def not_missed(value_1, value_2):
+  if value_1 is not MISSED:
+    return value_1
+  if value_2 is not MISSED:
+    return value_2
+  raise BothMissedException()
+
+
 class _Retry(object):
-  def __init__(self):
-    self.backoff_base = DEFAULT_BACKOFF_BASE
-    self.backoff_multiplier = DEFAULT_BACKOFF_MULTIPLIER
-    self.backoff_threshold = DEFAULT_BACKOFF_THRESHOLD
-    self.max_retries = DEFAULT_MAX_RETRIES
-    self.retrying_timeout = DEFAULT_RETRYING_TIMEOUT
-    self.retry_on_exception = DEFAULT_RETRY_ON_EXCEPTION
+  def __init__(self, backoff_base=MISSED, backoff_multiplier=MISSED,
+               backoff_threshold=MISSED, max_retries=MISSED,
+               retrying_timeout=MISSED, retry_on_exception=MISSED):
+    """
+    Args:
+      backoff_base: a number to use in backoff calculation.
+      backoff_multiplier: a number indicating initial backoff.
+      backoff_threshold: a number indicating maximum backoff.
+      max_retries: an integer indicating maximum number of retries.
+      retrying_timeout: a number indicating number of seconds after which
+        retrying should be stopped.
+      retry_on_exception: a function receiving one argument: exception object
+        and returning True if retry makes sense for such exception.
+        Alternatively you can pass list of exception types for which
+        retry is needed.
+    """
+    self.backoff_base = not_missed(backoff_base, DEFAULT_BACKOFF_BASE)
+    self.backoff_multiplier = not_missed(backoff_multiplier,
+                                         DEFAULT_BACKOFF_MULTIPLIER)
+    self.backoff_threshold = not_missed(backoff_threshold,
+                                        DEFAULT_BACKOFF_THRESHOLD)
+    self.max_retries = not_missed(max_retries, DEFAULT_MAX_RETRIES)
+    self.retrying_timeout = not_missed(retrying_timeout,
+                                       DEFAULT_RETRYING_TIMEOUT)
+    self.retry_on_exception = not_missed(retry_on_exception,
+                                         DEFAULT_RETRY_ON_EXCEPTION)
 
   def __call__(self, func=None, backoff_base=MISSED, backoff_multiplier=MISSED,
                backoff_threshold=MISSED, max_retries=MISSED,
@@ -44,21 +75,20 @@ class _Retry(object):
     Returns:
       A wrapped function or self if function was omitted in arguments.
     """
-    if backoff_base is not MISSED:
-      self.backoff_base = backoff_base
-    if backoff_multiplier is not MISSED:
-      self.backoff_multiplier = backoff_multiplier
-    if backoff_threshold is not MISSED:
-      self.backoff_threshold = backoff_threshold
-    if max_retries is not MISSED:
-      self.max_retries = max_retries
-    if retrying_timeout is not MISSED:
-      self.retrying_timeout = retrying_timeout
-    if retry_on_exception is not MISSED:
-      self.retry_on_exception = retry_on_exception
-
     if func is None:
-      return self
+      return _Retry(
+        backoff_base=backoff_base,
+        backoff_multiplier=backoff_multiplier,
+        backoff_threshold=backoff_threshold,
+        max_retries=max_retries,
+        retrying_timeout=retrying_timeout,
+        retry_on_exception=retry_on_exception
+      )
+
+    args = (backoff_base, backoff_multiplier, backoff_threshold, max_retries,
+            retrying_timeout, retry_on_exception)
+    if any((argument is not MISSED) for argument in args):
+      raise TypeError('You should pass either func or parameters')
 
     @functools.wraps(func)
     def wrapped(*args, **kwargs):
