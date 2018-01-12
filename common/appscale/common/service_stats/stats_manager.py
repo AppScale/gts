c89860f46b43de1dc320af0e1c7d41f40fae2e32
Anton Leonov <jeanleonov@gmail.com>
2018-01-12 18:14:05 +0200

    Doc strings

--- common/appscale/common/service_stats/stats_manager.py
+++ common/appscale/common/service_stats/stats_manager.py
@@ -112,6 +112,21 @@ def random_finish():
 
 
 class ServiceStats(object):
+  """
+  Collects stats about requests handled by a service.
+  Service should report any request it starts working on using:
+    start_request(request_info_dict)
+  After finishing request service should call:
+    finalize_request(request_no, result_info_dict)
+  (its recommended to do this in finally section of try-except statement).
+   
+  To retrieve cumulative stats call:
+    get_cumulative_stats()
+  To retrieve statistics for recent requests use one of:
+    get_recent(age)
+    scroll_recent(cursor)
+  """
+
   DEFAULT_HISTORY_SIZE = 1000
 
   DEFAULT_MAX_REQUEST_AGE = 60 * 60 * 2  # Force clean requests older than 2h
@@ -125,17 +140,29 @@ class ServiceStats(object):
                request_fields=DEFAULT_REQUEST_FIELDS,
                cumulative_counters=DEFAULT_CUMULATIVE_COUNTERS,
                metrics_for_recent=DEFAULT_METRICS_MAP):
+    """ Initialises an instance of ServiceStats.
+    
+    Args:
+      service_name: a str representing name of service.
+      history_size: a number of recent requests to store.
+      force_clean_after: seconds to wait before removing started requests.
+      lock_context_manager: an instance of context manager to use for managing
+        concurrent execution of requests.
+      request_fields: a list of request property names.
+      cumulative_counters: a dictionary describing cumulative counters config.
+      metrics_for_recent: a dictionary containing metrics 
+        which should be computed for recent requests.
+    """
 
     now = _now()
     self._service_name = service_name
-
     self._request_info_class = self._generate_request_model(request_fields)
 
     # Initialize properties for tracking latest N requests
     self._lock = lock_context_manager
     self._last_request_no = 0
     self._current_requests = {}  # {request_no: RequestInfo()}
-    self._finished_requests = []  # [RequestInfo()] up
+    self._finished_requests = []  # circular list containing recent N requests
 
     # Configure parameters limiting memory usage
     self._history_size = history_size
@@ -152,10 +179,12 @@ class ServiceStats(object):
 
   @property
   def service_name(self):
+    """ Name of service """
     return self._service_name
 
   @property
   def current_requests(self):
+    """ Number of currently running requests """
     return len(self._current_requests)
 
   def _generate_request_model(self, request_fields):
@@ -247,7 +276,7 @@ class ServiceStats(object):
       # Counters config can contain following types of items:
       #  - str->Matcher
       #  - Categorizer->Matcher
-      #  - Categorizer->nested config
+      #  - Categorizer->nested config with the same structure
 
       if isinstance(counter_pair[1], matchers.RequestMatcher):
         # Stop as soon as possible if we know that matcher doesn't match
@@ -275,15 +304,33 @@ class ServiceStats(object):
         category_counters[category] += 1
 
   def get_cumulative_counters(self):
+    """
+    Returns:
+      A dictionary containing current value of cumulative counters.
+    """
     return copy.deepcopy(self._cumulative_counters)
 
   def get_recent(self, for_last_seconds=None):
-    """  """
+    """ Provides value of metrics for recent requests which were finished
+    not earlier than for_last_seconds seconds ago.
+    
+    Args:
+      for_last_seconds: a number of seconds.
+    Returns:
+      a dictionary containing value of metrics for recent requests.
+    """
     cursor = _now() - for_last_seconds if for_last_seconds else None
     return self.scroll_recent(cursor=cursor)
 
   def scroll_recent(self, cursor=None):
-    """  """
+    """ Provides value of metrics for recent requests which were finished
+    after specified timestamp (cursor).
+    
+    Args:
+      cursor: a unix timestamp (in ms)
+    Returns:
+      a dictionary containing value of metrics for recent requests.
+    """
     requests = self._get_requests(since=cursor)
     stats = self._render_recent(self._metrics_for_recent_config, requests)
     if not requests:
@@ -296,12 +343,20 @@ class ServiceStats(object):
     return stats
 
   def _render_recent(self, metrics_config, requests):
+    """ Computes configured metrics according to metrics_config for requests.
+    
+    Args:
+      metrics_config: a dictionary describing what metrics should be computed.
+      requests: a list of requests to compute metrics for.
+    Returns:
+      a dictionary containing computed metrics.
+    """
     stats_dict = {}
     for metric_pair in iteritems(metrics_config):
       # Metrics config can contain following types of items:
       #  - str->Metric
       #  - Categorizer->Metric
-      #  - Categorizer->nested config
+      #  - Categorizer->nested config with the same structure
 
       if isinstance(metric_pair[0], str):
         # Compute single metric if key is str
@@ -335,6 +390,13 @@ class ServiceStats(object):
     return stats_dict
 
   def _get_requests(self, since=None):
+    """ Selects requests which were finished since specified timestamp (in ms).
+    
+    Args:
+      since: a unix timestamp in ms.
+    Returns:
+      a list of requests finished since specified timestamp.
+    """
     if since is None:
       return self._finished_requests
     # Find the first element newer than 'since' using bisect
@@ -366,17 +428,24 @@ class ServiceStats(object):
 
 
 def _now():
+  """
+  Returns:
+    a unix timestamp in milliseconds.
+  """
   return int(time.time() * 1000)
 
 
 def _get_nested_dict(dictionary, key):
+  """ A util function for getting (and putting if missed) nested dictionary.
+  
+  Args:
+    dictionary: an instance of dict. 
+    key: a key.
+  Returns:
+    a dictionary gotten by key (newly created dict if it was missed). 
+  """
   if key not in dictionary:
     nested = defaultdict(int)
     dictionary[key] = nested
     return nested
   return dictionary[key]
-
-
-
-
-
