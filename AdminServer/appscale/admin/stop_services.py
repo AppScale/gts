051bc68acbde77a40acdb8b47f20586f3d1021a3
Chris Donati <chris.donati@appscale.com>
2018-07-31 17:39:21 -0700

    Stop services in a particular order
    This orders services roughly by their dependencies. The services
    that rely on other services are stopped before the underlying
    backend services.
    
    This is mainly meant to address a Monit issue where the controller
    is designated as "Initializing" sometime during the stop process,
    preventing any other services from being stopped until its status
    is resolved.

--- AdminServer/appscale/admin/stop_services.py
+++ AdminServer/appscale/admin/stop_services.py
@@ -6,6 +6,57 @@ import time
 from appscale.common.monit_interface import MonitOperator, MonitStates
 
 
+def order_services(running_services):
+  """ Arranges a list of running services in the order they should be stopped.
+
+  Args:
+    running_services: A list of strings specifying running services.
+  Returns:
+    A list of strings specifying services that should be stopped.
+  """
+  service_order = [
+    # First, stop the services that manage other services.
+    'controller',
+    'admin_server',
+    'appmanagerserver',
+
+    # Next, stop application runtime instances.
+    'app___',
+    'api-server_',
+
+    # Next, stop services that depend on other services.
+    'app_haproxy',
+    'service_haproxy',
+    'blobstore',
+    'celery-',
+    'flower',
+    'groomer_service',
+    'hermes',
+    'iaas_manager',
+    'log_service',
+    'taskqueue-',
+    'transaction_groomer',
+    'uaserver',
+
+    # Finally, stop the underlying backend services.
+    'cassandra',
+    'ejabberd',
+    'memcached',
+    'rabbitmq',
+    'zookeeper'
+  ]
+
+  ordered_services = []
+  for service_type in service_order:
+    relevant_entries = [service for service in running_services
+                        if service.startswith(service_type)]
+    for entry in relevant_entries:
+      index = running_services.index(entry)
+      ordered_services.append(running_services.pop(index))
+
+  return ordered_services + running_services
+
+
 def main():
   """ Tries to stop all Monit services until they are stopped. """
   monit_operator = MonitOperator()
@@ -28,7 +79,7 @@ def main():
       print('Stopped {}/{} services'.format(stopped_count, len(services)))
 
     try:
-      service = next((service for service in sorted(running.keys())
+      service = next((service for service in order_services(running.keys())
                       if services[service] != MonitStates.PENDING))
       subprocess.Popen(['monit', 'stop', service])
     except StopIteration:
