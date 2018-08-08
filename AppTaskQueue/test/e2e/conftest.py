b2d7364dcb0f5abf241868f53b95c6ba9cab6f5d
Anton Leonov <jeanleonov@gmail.com>
2018-08-08 13:43:03 +0300

    First few e2e tests for REST and Protobuffer APIs

---
+++ AppTaskQueue/test/e2e/conftest.py
@@ -0,0 +1,101 @@
+import asyncio
+import functools
+import json
+import os
+import time
+
+import kazoo.client
+import pytest
+from kazoo.exceptions import NoNodeError
+
+from helpers import api_helper, taskqueue_service_pb2
+
+
+TEST_PROJECT = os.environ['TEST_PROJECT']
+PG_DSN_NODE = f'/appscale/projects/{TEST_PROJECT}/postgres_dsn'
+PROJECT_NODE = f'/appscale/projects/{TEST_PROJECT}'
+PROJECT_QUEUES_NODE = f'/appscale/projects/{TEST_PROJECT}/queues'
+PROJECT_QUEUES_CONFIG = {
+  'queue': {
+    'pull-queue-a': {'mode': 'pull'},
+    'pull-queue-b': {'mode': 'pull'},
+    'pull-queue-5-retry': {'mode': 'pull', 'task_retry_limit': 5},
+    'pull-queue-3-retry': {'mode': 'pull', 'task_retry_limit': 3},
+    'push-queue': {}
+  }
+}
+PROJECT_QUEUES_CONFIG_BYTES = bytes(json.dumps(PROJECT_QUEUES_CONFIG), 'utf-8')
+
+POSTGRES = 'postgres'
+CASSANDRA = 'cassandra'
+
+
+def async_test(test):
+  @functools.wraps(test)
+  def wrapper(*args, **kwargs):
+    coro = asyncio.coroutine(test)
+    future = coro(*args, **kwargs)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(future)
+  return wrapper
+
+
+def pytest_addoption(parser):
+  parser.addoption('--tq-locations', nargs='+',
+                   help='TCP addresses of TaskQueue')
+  parser.addoption('--zk-location',
+                   help='TCP address of Zookeeper')
+
+
+@pytest.fixture(scope='session')
+def taskqueue(request):
+  """ Initializes TaskQueue access point
+  """
+  tq_locations = request.config.getoption('--tq-locations')
+  return api_helper.TaskQueue(tq_locations, TEST_PROJECT)
+
+
+@pytest.fixture(scope='session')
+def pull_queues_backend(request):
+  """ Reports what backend is used for Pull Queues
+  """
+  # Configure Zookeeper client
+  zk_location = request.config.getoption('--zk-location')
+  zk_client = kazoo.client.KazooClient(hosts=zk_location)
+  zk_client.start()
+  # Check if PG_DSN node is specified for the project
+  try:
+    zk_client.get(PG_DSN_NODE)
+  except NoNodeError:
+    return CASSANDRA
+  finally:
+    zk_client.stop()
+  return POSTGRES
+
+
+@pytest.fixture(scope='session', autouse=True)
+def init_queues_config(request):
+  # Configure Zookeeper client
+  zk_location = request.config.getoption('--zk-location')
+  zk_client = kazoo.client.KazooClient(hosts=zk_location)
+  zk_client.start()
+
+  # Ensure queue configs are set
+  zk_client.ensure_path(PROJECT_QUEUES_NODE)
+  zk_client.set(PROJECT_QUEUES_NODE, PROJECT_QUEUES_CONFIG_BYTES)
+  zk_client.stop()
+
+  # Give Taskqueue some time to update queues config
+  time.sleep(3)
+
+
+@pytest.fixture(autouse=True)
+def purge_queues(taskqueue):
+  # Nothing to setup
+  yield
+  # Cleanup after every test
+  for queue_name, info in PROJECT_QUEUES_CONFIG['queue'].items():
+    if info.get('mode') == 'pull':
+      purge_request = taskqueue_service_pb2.TaskQueuePurgeQueueRequest()
+      purge_request.queue_name = bytes(queue_name, 'utf-8')
+      taskqueue.protobuf_sync('PurgeQueue', purge_request)
---
+++ AppTaskQueue/test/e2e/test_protobuffer_api.py
@@ -0,0 +1,268 @@
+import asyncio
+import time
+
+import pytest
+
+from conftest import async_test, TEST_PROJECT, POSTGRES, CASSANDRA
+from helpers import taskqueue_service_pb2
+
+
+@async_test
+async def test_add_lease_prolong_delete(taskqueue):
+  # Initialize tasks
+  queue_str = 'pull-queue-b'
+  queue_bytes = bytes(queue_str, 'utf8')
+  tasks = []
+  for n in range(10):
+    add_task = taskqueue_service_pb2.TaskQueueAddRequest()
+    add_task.app_id = bytes(TEST_PROJECT, 'utf8')
+    add_task.queue_name = queue_bytes
+    add_task.mode = taskqueue_service_pb2.TaskQueueMode.PULL
+    add_task.task_name = b'task-%d' % n
+    add_task.body = b'some-payload-%d' % n
+    add_task.eta_usec = 0
+    tasks.append(add_task)
+  bulk_add = taskqueue_service_pb2.TaskQueueBulkAddRequest()
+  bulk_add.add_request.extend(tasks[:5])
+
+  # Add tasks using single and bulk add
+  await taskqueue.protobuf('BulkAdd', bulk_add)
+  asyncio_tasks = []
+  for task in tasks[5:]:
+    asyncio_tasks.append(taskqueue.protobuf('Add', task))
+  await asyncio.wait(asyncio_tasks)
+  remote_time = await taskqueue.remote_time_usec()
+
+  # Lease 10 tasks for 5 second
+  lease_req = taskqueue_service_pb2.TaskQueueQueryAndOwnTasksRequest()
+  lease_req.queue_name = queue_bytes
+  lease_req.lease_seconds = 5
+  lease_req.max_tasks = 10
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  # Make sure eta_usec is ~5s greater than current remote_time
+  assert all(
+    task.eta_usec == pytest.approx(remote_time + 5_000_000, abs=300_000)
+    for task in leased.task
+  )
+
+  # Make a pause greater than absolute precision of approx comparison
+  time.sleep(0.6)
+  remote_time = await taskqueue.remote_time_usec()
+
+  # Prolong lease for 6 seconds
+  prolong_requests = []
+  for task in leased.task:
+    req = taskqueue_service_pb2.TaskQueueModifyTaskLeaseRequest()
+    req.queue_name = queue_bytes
+    req.task_name = task.task_name
+    req.eta_usec = task.eta_usec
+    req.lease_seconds = 6
+    prolong_requests.append(taskqueue.protobuf('ModifyTaskLease', req))
+  # Wait for multiple responses
+  done_tasks, _ = await asyncio.wait(prolong_requests)
+  responses = [asyncio_task.result() for asyncio_task in done_tasks]
+  actual = [resp.updated_eta_usec for resp in responses]
+  expected = [pytest.approx(remote_time + 6_000_000, abs=300_000)] * 10
+  # Make sure updated_eta_usec is 6s greater than start_time
+  assert actual == expected
+
+  # Verify listed tasks
+  listed = await taskqueue.rest('GET', path_suffix=f'/{queue_str}/tasks')
+  actual = [int(task['leaseTimestamp']) for task in listed.json['items']]
+  expected = [pytest.approx(remote_time + 6_000_000, abs=300_000)] * 10
+  # Make sure leaseTimestamp is what we expect
+  assert actual == expected
+  assert (  # Make sure all tasks are in place
+    set(task['id'] for task in listed.json['items'])
+    == set(str(task.task_name, 'utf8') for task in tasks)
+  )
+
+  # Delete tasks
+  req = taskqueue_service_pb2.TaskQueueDeleteRequest()
+  req.queue_name = queue_bytes
+  req.task_name.extend([task.task_name for task in leased.task])
+  await taskqueue.protobuf('Delete', req)
+  # Verify that queue is empty
+  listed = await taskqueue.rest('GET', path_suffix=f'/{queue_str}/tasks')
+  assert listed.json == {'kind': 'taskqueues#tasks'}  # items should be missing
+
+
+@async_test
+async def test_add_lease_retry_retry_delete_pg(taskqueue, pull_queues_backend):
+  if pull_queues_backend == CASSANDRA:
+    pytest.skip('Skipped for Cassandra backend')
+
+  # Initialize tasks
+  queue_str = 'pull-queue-5-retry'
+  queue_bytes = bytes(queue_str, 'utf8')
+  add_tasks = []
+  for n in range(4):
+    add_task = taskqueue_service_pb2.TaskQueueAddRequest()
+    add_task.app_id = bytes(TEST_PROJECT, 'utf8')
+    add_task.queue_name = queue_bytes
+    add_task.mode = taskqueue_service_pb2.TaskQueueMode.PULL
+    add_task.task_name = b'task-%d' % n
+    add_task.body = b'some-payload-%d' % n
+    add_task.eta_usec = 0
+    add_tasks.append(add_task)
+  bulk_add = taskqueue_service_pb2.TaskQueueBulkAddRequest()
+  bulk_add.add_request.extend(add_tasks)
+  start_time = await taskqueue.remote_time_usec()
+
+  # Add tasks using single and bulk add
+  await taskqueue.protobuf('BulkAdd', bulk_add)
+
+  # Lease 4 tasks for a second
+  lease_req = taskqueue_service_pb2.TaskQueueQueryAndOwnTasksRequest()
+  lease_req.queue_name = queue_bytes
+  lease_req.lease_seconds = 1
+  lease_req.max_tasks = 4
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == [1, 1, 1, 1]
+  # Try to lease 4 tasks for 3 seconds
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == []
+
+  # Give 2 seconds for lease to expire
+  time.sleep(2)
+
+  # Lease 2 tasks for a second (retry)
+  lease_req.max_tasks = 2
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == [2, 2]
+
+  # Lease 2 tasks for a second (retry)
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == [2, 2]
+
+  # Try to lease 2 tasks for a second
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == []
+
+  # Give 2 seconds for lease to expire
+  time.sleep(2)
+
+  # Try to lease 3 tasks for 1 second
+  lease_req.max_tasks = 3
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == [3, 3, 3]
+
+  # Give 2 seconds for lease to expire
+  time.sleep(2)
+
+  # Try to lease 3 tasks for a second
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == [3, 4, 4]
+
+  # Verify listed tasks
+  listed = await taskqueue.rest('GET', path_suffix=f'/{queue_str}/tasks')
+  sorting_key = lambda item: (item['retry_count'], int(item['leaseTimestamp']))
+  tasks = sorted(listed.json['items'], key=sorting_key)
+  actual = [(task['retry_count'], int(task['leaseTimestamp'])) for task in tasks]
+  expected = [
+    (3, pytest.approx(start_time + 5_000_000, abs=700_000)),
+    (3, pytest.approx(start_time + 7_000_000, abs=700_000)),
+    (4, pytest.approx(start_time + 7_000_000, abs=700_000)),
+    (4, pytest.approx(start_time + 7_000_000, abs=700_000)),
+  ]
+  assert actual == expected
+
+  # Delete tasks
+  req = taskqueue_service_pb2.TaskQueueDeleteRequest()
+  req.queue_name = queue_bytes
+  req.task_name.extend([task.task_name for task in add_tasks])
+  await taskqueue.protobuf('Delete', req)
+  # Verify that queue is empty
+  listed = await taskqueue.rest('GET', path_suffix=f'/{queue_str}/tasks')
+  assert listed.json == {'kind': 'taskqueues#tasks'}  # items should be missing
+
+
+@async_test
+async def test_add_lease_retry_retry_delete_cassandra(taskqueue, pull_queues_backend):
+  if pull_queues_backend == POSTGRES:
+
+    pytest.skip('Skipped for Postgres backend')
+
+  # Initialize tasks
+  queue_str = 'pull-queue-5-retry'
+  queue_bytes = bytes(queue_str, 'utf8')
+  add_tasks = []
+  for n in range(4):
+    add_task = taskqueue_service_pb2.TaskQueueAddRequest()
+    add_task.app_id = bytes(TEST_PROJECT, 'utf8')
+    add_task.queue_name = queue_bytes
+    add_task.mode = taskqueue_service_pb2.TaskQueueMode.PULL
+    add_task.task_name = b'task-%d' % n
+    add_task.body = b'some-payload-%d' % n
+    add_task.eta_usec = 0
+    add_tasks.append(add_task)
+  bulk_add = taskqueue_service_pb2.TaskQueueBulkAddRequest()
+  bulk_add.add_request.extend(add_tasks)
+  start_time = await taskqueue.remote_time_usec()
+
+  # Add tasks using single and bulk add
+  await taskqueue.protobuf('BulkAdd', bulk_add)
+
+  # Lease 4 tasks for a second
+  lease_req = taskqueue_service_pb2.TaskQueueQueryAndOwnTasksRequest()
+  lease_req.queue_name = queue_bytes
+  lease_req.lease_seconds = 1
+  lease_req.max_tasks = 4
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == [0, 0, 0, 0]
+  # Try to lease 4 tasks for 3 seconds
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == []
+
+  # Give 5 seconds for lease to expire
+  time.sleep(5)
+
+  # Lease 2 tasks for a second (retry)
+  lease_req.max_tasks = 2
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == [1, 1]
+
+  # Lease 2 tasks for a second (retry)
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == [1, 1]
+
+  # Try to lease 2 tasks for a second
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == []
+
+  # Give 5 seconds for lease to expire
+  time.sleep(5)
+
+  # Try to lease 3 tasks for a second
+  lease_req.max_tasks = 3
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == [2, 2, 2]
+
+  # Give 5 seconds for lease to expire
+  time.sleep(5)
+
+  # Try to lease 3 tasks for a second
+  leased = await taskqueue.protobuf('QueryAndOwnTasks', lease_req)
+  assert [task.retry_count for task in leased.task] == [2, 3, 3]
+
+  # Verify listed tasks
+  listed = await taskqueue.rest('GET', path_suffix=f'/{queue_str}/tasks')
+  sorting_key = lambda item: (item['retry_count'], int(item['leaseTimestamp']))
+  tasks = sorted(listed.json['items'], key=sorting_key)
+  actual = [(task['retry_count'], int(task['leaseTimestamp'])) for task in tasks]
+  expected = [
+    (3, pytest.approx(start_time + 11_000_000, abs=900_000)),
+    (3, pytest.approx(start_time + 16_000_000, abs=900_000)),
+    (4, pytest.approx(start_time + 16_000_000, abs=900_000)),
+    (4, pytest.approx(start_time + 16_000_000, abs=900_000)),
+  ]
+  assert actual == expected
+
+  # Delete tasks
+  req = taskqueue_service_pb2.TaskQueueDeleteRequest()
+  req.queue_name = queue_bytes
+  req.task_name.extend([task.task_name for task in add_tasks])
+  await taskqueue.protobuf('Delete', req)
+  # Verify that queue is empty
+  listed = await taskqueue.rest('GET', path_suffix=f'/{queue_str}/tasks')
+  assert listed.json == {'kind': 'taskqueues#tasks'}  # items should be missing
