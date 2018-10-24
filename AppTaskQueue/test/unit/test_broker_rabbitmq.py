563c62d535baeb79ace26acacb2b7224f25ab50c
Chris Donati <chris.donati@appscale.com>
2018-10-23 23:41:55 -0700

    Connect to local RabbitMQ instance
    Newer versions of RabbitMQ only allow "guest" clients via
    localhost.

--- AppTaskQueue/test/unit/test_broker_rabbitmq.py
+++ AppTaskQueue/test/unit/test_broker_rabbitmq.py
@@ -2,9 +2,6 @@
 
 import unittest
 
-from flexmock import flexmock
-
-from appscale.common import file_io
 from appscale.taskqueue.brokers import rabbitmq
 
 
@@ -13,11 +10,7 @@ class TestBrokerRabbitMQ(unittest.TestCase):
   A set of test cases for the RabbitMQ broker.
   """
   def test_get_rabbitmq_location(self):
-    flexmock(file_io) \
-       .should_receive("read").and_return("192.168.0.1")
-    flexmock(file_io) \
-       .should_receive("write").and_return(None)
-    expected = "amqp://guest:guest@192.168.0.1:5672//"
+    expected = "amqp://guest:guest@localhost:5672//"
     self.assertEquals(rabbitmq.get_connection_string(), expected)
 
 if __name__ == "__main__":
