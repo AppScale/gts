575444fc132e95116d71839847306a219f2761ea
Steve Jones <steve.jones@appscale.com>
2019-02-12 10:15:15 -0800

    Ignore Python arg when parsing login server port

--- AdminServer/appscale/admin/instance_manager/instance.py
+++ AdminServer/appscale/admin/instance_manager/instance.py
@@ -229,7 +229,7 @@ def get_login_server(instance):
     return None
 
   for index, arg in enumerate(args):
-    if '--login_server=' in arg:
+    if '--login_server=' in arg and 'python_api_server_flag' not in arg:
       return arg.split('=', 1)[1]
 
     if arg == '--login_server':
