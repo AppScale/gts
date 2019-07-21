224259e650dfbe8f605b788f9c6171e0aac01b9b
Chris Donati <chris.donati@appscale.com>
2019-07-21 15:09:13 -0700

    Update some builtins from the 1.9.76 SDK
    This includes updates from mapreduce and datastore_admin.

--- AppServer/google/appengine/ext/mapreduce/static/status.js
+++ AppServer/google/appengine/ext/mapreduce/static/status.js
@@ -676,14 +676,14 @@ function refreshJobDetail(jobId, detail) {
 
     var status = (shard.active ? 'running' : shard.result_status) || 'unknown';
     row.append($('<td>').text(status));
+    row.append($('<td>').text(getElapsedTimeString(
+        detail.start_timestamp_ms, shard.updated_timestamp_ms)));
+    row.append($('<td>').text(shard.counters.Entities));
+    row.append($('<td>').text(shard.counters.Bytes));
 
-    // TODO: Set colgroup width for shard description.
     row.append($('<td>').text(shard.shard_description));
 
     row.append($('<td>').text(shard.last_work_item || 'Unknown'));
-
-    row.append($('<td>').text(getElapsedTimeString(
-        detail.start_timestamp_ms, shard.updated_timestamp_ms)));
 
     row.appendTo(mapperBody);
   });
