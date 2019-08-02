62e7d19d5f1d8285cbecfb30cea3e9fe4e21c43d
Honcharov12 <honcharov12@gmail.com>
2019-08-02 16:57:59 +0300

    Fix typo and code refactoring

--- AppDB/appscale/datastore/scripts/ua_server_backup.py
+++ AppDB/appscale/datastore/scripts/ua_server_backup.py
@@ -1,6 +1,7 @@
 """ UA Server data backup. """
 
 import argparse
+import csv
 import errno
 import logging
 import os
@@ -20,6 +21,8 @@ from appscale.datastore.utils import tornado_synchronous
 # The location where the backup files will be stored.
 BACKUP_FILE_LOCATION = "/opt/appscale/backups/"
 
+logger = logging.getLogger(__name__)
+
 
 @tornado_synchronous
 @gen.coroutine
@@ -27,7 +30,7 @@ def get_table_sync(datastore, table_name, schema):
   """ Gets data from datastore.
 
   Args:
-    datastore: Datastore entity.
+    datastore: Cassandra adapter.
     table_name: Table name in datastore.
     schema: Table schema.
   """
@@ -51,30 +54,14 @@ def create_backup_dir(backup_dir):
 
   Args:
     backup_dir: Backup directory name.
-  Returns:
-    True if directory was created or existed before, false otherwise.
   """
-  logger = logging.getLogger(__name__)
   try:
     os.makedirs(backup_dir)
-    logger.info("Backup dir created: {0}".format(backup_dir))
-    return True
-  except OSError, os_error:
-    if os_error.errno == errno.EEXIST:
-      logger.warn("OSError: Backup directory already exists.")
-      return True
-    elif os_error.errno == errno.ENOSPC:
-      logger.error("OSError: No space left to create backup directory.")
-      logger.error(os_error.message)
-      return False
-    elif os_error.errno == errno.EROFS:
-      logger.error("OSError: READ-ONLY filesystem detected.")
-      logger.error(os_error.message)
-      return False
-  except IOError, io_error:
-    logger.error("IOError while creating backup dir.")
-    logger.error(io_error.message)
-    return False
+  except OSError as os_error:
+    if os_error.errno != errno.EEXIST:
+      raise
+
+  logger.info("Backup dir created: {0}".format(backup_dir))
 
 def main():
   logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
@@ -82,53 +69,54 @@ def main():
   parser = argparse.ArgumentParser(description='Backup UA Server data.')
   parser.add_argument(
     '-v', '--verbose', action='store_true', help='Output debug-level logging')
-  parser.add_argument('-t', '--type', help='Datastore type')
   args = parser.parse_args()
 
   if args.verbose:
     logging.getLogger('appscale').setLevel(logging.DEBUG)
 
   datastore_type = 'cassandra'
-  if args.type:
-    datastore_type = args.type
 
   ERROR_CODES = appscale_datastore.DatastoreFactory.error_codes()
-  valid_datastores = appscale_datastore.DatastoreFactory.valid_datastores()
-  if datastore_type not in valid_datastores:
-    raise Exception('{} not in valid datastores ({})'.
-                    format(datastore_type, valid_datastores))
 
   db = appscale_datastore.DatastoreFactory.getDatastore(datastore_type)
 
   # Keep trying until it gets the schema.
-  timeout = 5
-  while 1:
+  backoff = 5
+  retries = 3
+  while retries >= 0:
     try:
       user_schema = db.get_schema_sync(USERS_TABLE)
     except AppScaleDBConnectionError:
-      time.sleep(timeout)
+      retries -= 1
+      time.sleep(backoff)
       continue
 
     if user_schema[0] in ERROR_CODES:
       user_schema = user_schema[1:]
     else:
-      time.sleep(timeout)
+      retries -= 1
+      time.sleep(backoff)
       continue
     break
+
+  # If no response from cassandra
+  if retries == -1:
+    raise AppScaleDBConnectionError('No response from cassandra.')
 
   schema_cols_num = len(USERS_SCHEMA)
   table = get_table_sync(db, USERS_TABLE, user_schema)[1:]
   reshaped_table = reshape(table, schema_cols_num)
 
-  is_created = create_backup_dir(BACKUP_FILE_LOCATION)
-
-  if not is_created:
-    return
+  try:
+    create_backup_dir(BACKUP_FILE_LOCATION)
+  except OSError as os_error:
+    logger.error('OSError occurred!')
+    logger.error(os_error.message)
+    raise
 
   backup_timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
   output = '{0}ua_server_{1}.csv'.format(BACKUP_FILE_LOCATION, backup_timestamp)
 
   with open(output, 'w') as fout:
-    fout.write(','.join(USERS_SCHEMA) + '\n')
-    for row in reshaped_table:
-      fout.write(','.join(row) + '\n')
+    writer = csv.writer(fout, delimiter=',')
+    writer.writerows(reshaped_table)
--- AppDB/appscale/datastore/scripts/ua_server_restore.py
+++ AppDB/appscale/datastore/scripts/ua_server_restore.py
@@ -1,6 +1,7 @@
 """ UA Server data restore. """
 
 import argparse
+import csv
 import logging
 import time
 
@@ -30,10 +31,9 @@ def put_entity_sync(datastore, table_name, user, schema, array):
 def main():
   logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
 
-  parser = argparse.ArgumentParser(description='Backup UA Server data.')
+  parser = argparse.ArgumentParser(description='Restore UA Server data.')
   parser.add_argument(
     '-v', '--verbose', action='store_true', help='Output debug-level logging')
-  parser.add_argument('-t', '--type', help='Datastore type')
   parser.add_argument(
     '-i', '--input', help='File with UA Server backup', required=True)
   args = parser.parse_args()
@@ -42,39 +42,38 @@ def main():
     logging.getLogger('appscale').setLevel(logging.DEBUG)
 
   datastore_type = 'cassandra'
-  if args.type:
-    datastore_type = args.type
 
   ERROR_CODES = appscale_datastore.DatastoreFactory.error_codes()
-  valid_datastores = appscale_datastore.DatastoreFactory.valid_datastores()
-  if datastore_type not in valid_datastores:
-    raise Exception('{} not in valid datastores ({})'.
-                    format(datastore_type, valid_datastores))
 
   db = appscale_datastore.DatastoreFactory.getDatastore(datastore_type)
 
   # Keep trying until it gets the schema.
-  timeout = 5
-  while 1:
+  backoff = 5
+  retries = 3
+  while retries >= 0:
     try:
       user_schema = db.get_schema_sync(USERS_TABLE)
     except AppScaleDBConnectionError:
-      time.sleep(timeout)
+      retries -= 1
+      time.sleep(backoff)
       continue
 
     if user_schema[0] in ERROR_CODES:
       user_schema = user_schema[1:]
     else:
-      time.sleep(timeout)
+      retries -= 1
+      time.sleep(backoff)
       continue
     break
+
+  # If no response from cassandra
+  if retries == -1:
+    raise AppScaleDBConnectionError('No response from cassandra.')
 
   input = args.input
 
   with open(input, 'r') as fin:
-    # Skip headers
-    next(fin)
+    reader = csv.reader(fin, delimiter=',')
     # Iterate through all users in file
-    for line in fin:
-      array = line.rstrip().split(',')
-      put_entity_sync(db, USERS_TABLE, array[0], user_schema, array)
+    for row in reader:
+      put_entity_sync(db, USERS_TABLE, row[0], user_schema, row)
