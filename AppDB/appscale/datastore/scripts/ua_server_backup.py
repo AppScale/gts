60b42fc814e8a4089006460383fda3b7731fbf64
Honcharov12 <honcharov12@gmail.com>
2019-10-01 18:23:12 +0300

    Add postgres option to UA Server

--- AppDB/appscale/datastore/scripts/ua_server_backup.py
+++ AppDB/appscale/datastore/scripts/ua_server_backup.py
@@ -2,14 +2,19 @@
 
 import argparse
 import csv
+import datetime
 import errno
 import logging
 import os
 import time
 
+from kazoo.client import KazooClient
+import psycopg2
 from tornado import gen
 
-from appscale.common.constants import LOG_FORMAT
+
+from appscale.common import appscale_info, retrying
+from appscale.common.constants import LOG_FORMAT, ZK_PERSISTENT_RECONNECTS
 from appscale.datastore import appscale_datastore
 from appscale.datastore.dbconstants import (
   AppScaleDBConnectionError,
@@ -23,7 +28,82 @@ BACKUP_FILE_LOCATION = "/opt/appscale/backups/"
 
 logger = logging.getLogger(__name__)
 
+zk_client = None
 
+table_name = "ua_server"
+
+
+def is_connection_error(err):
+  """ This function is used as retry criteria.
+  It also makes possible lazy load of psycopg2 package.
+
+  Args:
+    err: an instance of Exception.
+  Returns:
+    True if error is related to connection, False otherwise.
+  """
+  from psycopg2 import InterfaceError
+  return isinstance(err, InterfaceError)
+
+
+retry_pg_connection = retrying.retry(
+    retrying_timeout=10, retry_on_exception=is_connection_error
+)
+
+
+class PostgresConnectionWrapper(object):
+  """ Implements automatic reconnection to Postgresql server. """
+
+  def __init__(self, dsn=None):
+    self._dsn = dsn
+    self._connection = None
+
+  def set_dsn(self, dsn):
+    """ Resets PostgresConnectionWrapper to use new DSN string.
+    Args:
+      dsn: a str representing Postgres DSN string.
+    """
+    if self._connection and not self._connection.closed:
+      self.close()
+      self._connection = None
+    self._dsn = dsn
+
+  def get_connection(self):
+    """ Provides postgres connection. It can either return existing
+    working connection or establish new one.
+    Returns:
+      An instance of psycopg2 connection.
+    """
+    if not self._connection or self._connection.closed:
+      logger.info('Establishing new connection to Postgres server')
+      self._connection = psycopg2.connect(dsn=self._dsn)
+    return self._connection
+
+  def close(self):
+    """ Closes psycopg2 connection.
+    """
+    return self._connection.close()
+
+
+pg_connection_wrapper = None
+
+
+def connect_to_postgres(zk_client):
+  global pg_connection_wrapper
+  global_dsn_node = '/appscale/ua_server/postgres_dsn'
+  if zk_client.exists(global_dsn_node):
+    pg_dsn = zk_client.get(global_dsn_node)
+    logger.info('Using PostgreSQL as a backend for UA Server')
+  else:
+    pg_dsn = None
+    logger.info('Using Cassandra as a backend for UA Server')
+  if pg_dsn:
+    pg_connection_wrapper = (
+        PostgresConnectionWrapper(dsn=pg_dsn[0])
+    )
+
+
+@retry_pg_connection
 @tornado_synchronous
 @gen.coroutine
 def get_table_sync(datastore, table_name, schema):
@@ -34,6 +114,17 @@ def get_table_sync(datastore, table_name, schema):
     table_name: Table name in datastore.
     schema: Table schema.
   """
+  if pg_connection_wrapper:
+    pg_connection = pg_connection_wrapper.get_connection()
+    with pg_connection:
+      with pg_connection.cursor() as pg_cursor:
+        pg_cursor.execute(
+          'SELECT * FROM "{table}" '
+          .format(table=table_name)
+        )
+        result = pg_cursor.fetchall()
+    raise gen.Return(result)
+
   result = yield datastore.get_table(table_name, schema)
   raise gen.Return(result)
 
@@ -63,6 +154,23 @@ def create_backup_dir(backup_dir):
 
   logger.info("Backup dir created: {0}".format(backup_dir))
 
+def prepare_for_backup(rows):
+  """ Converts date fields to timestamp and application list to str.
+
+  Args:
+    rows: A tuple of all rows in postgres database.
+  """
+  # todo: delete it after removal of Cassandra
+  for row in rows:
+    # 2 - 4 indexes of dates
+    row[2] = datetime.datetime.fromtimestamp(row[2])
+    row[3] = datetime.datetime.fromtimestamp(row[3])
+    row[4] = datetime.datetime.fromtimestamp(row[4])
+    # 5 index of applications list
+    if row[5]:
+      row[5] = row[5].split(':')
+
+
 def main():
   logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
 
@@ -73,6 +181,13 @@ def main():
 
   if args.verbose:
     logging.getLogger('appscale').setLevel(logging.DEBUG)
+
+  # Configure zookeeper and db access
+  zk_client = KazooClient(
+    hosts=','.join(appscale_info.get_zk_node_ips()),
+    connection_retry=ZK_PERSISTENT_RECONNECTS)
+  zk_client.start()
+  connect_to_postgres(zk_client)
 
   datastore_type = 'cassandra'
 
@@ -104,17 +219,26 @@ def main():
     raise AppScaleDBConnectionError('No response from cassandra.')
 
   schema_cols_num = len(USERS_SCHEMA)
-  table = get_table_sync(db, USERS_TABLE, user_schema)[1:]
-  reshaped_table = reshape(table, schema_cols_num)
+
+  if pg_connection_wrapper:
+    table = get_table_sync(db, table_name, user_schema)
+  else:
+    table = get_table_sync(db, USERS_TABLE, user_schema)[1:]
+    reshaped_table = reshape(table, schema_cols_num)
 
   create_backup_dir(BACKUP_FILE_LOCATION)
 
   backup_timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
-  output_file = '{0}ua_server_{1}.csv'.format(BACKUP_FILE_LOCATION, backup_timestamp)
+  output_file = '{0}ua_server_{1}.csv'.\
+    format(BACKUP_FILE_LOCATION, backup_timestamp)
 
   # v1 output format
   with open(output_file, 'w') as fout:
     writer = csv.DictWriter(fout, delimiter=',', fieldnames=USERS_SCHEMA)
     writer.writeheader()
-    rows = [dict(zip(USERS_SCHEMA, row)) for row in reshaped_table]
+    if pg_connection_wrapper:
+      rows = [dict(zip(USERS_SCHEMA, row)) for row in table]
+    else:
+      prepare_for_backup(reshaped_table)
+      rows = [dict(zip(USERS_SCHEMA, row)) for row in reshaped_table]
     writer.writerows(rows)
--- AppDB/appscale/datastore/scripts/ua_server_restore.py
+++ AppDB/appscale/datastore/scripts/ua_server_restore.py
@@ -2,12 +2,16 @@
 
 import argparse
 import csv
+import datetime
 import logging
 import time
 
+from kazoo.client import KazooClient
+import psycopg2
 from tornado import gen
 
-from appscale.common.constants import LOG_FORMAT
+from appscale.common import appscale_info, retrying
+from appscale.common.constants import LOG_FORMAT, ZK_PERSISTENT_RECONNECTS
 from appscale.datastore import appscale_datastore
 from appscale.datastore.dbconstants import (
   AppScaleDBConnectionError,
@@ -17,9 +21,87 @@ from appscale.datastore.dbconstants import (
 from appscale.datastore.utils import tornado_synchronous
 
 
+logger = logging.getLogger(__name__)
+
+zk_client = None
+
+table_name = "ua_server"
+
+
+def is_connection_error(err):
+  """ This function is used as retry criteria.
+  It also makes possible lazy load of psycopg2 package.
+
+  Args:
+    err: an instance of Exception.
+  Returns:
+    True if error is related to connection, False otherwise.
+  """
+  from psycopg2 import InterfaceError
+  return isinstance(err, InterfaceError)
+
+
+retry_pg_connection = retrying.retry(
+    retrying_timeout=10, retry_on_exception=is_connection_error
+)
+
+
+class PostgresConnectionWrapper(object):
+  """ Implements automatic reconnection to Postgresql server. """
+
+  def __init__(self, dsn=None):
+    self._dsn = dsn
+    self._connection = None
+
+  def set_dsn(self, dsn):
+    """ Resets PostgresConnectionWrapper to use new DSN string.
+    Args:
+      dsn: a str representing Postgres DSN string.
+    """
+    if self._connection and not self._connection.closed:
+      self.close()
+      self._connection = None
+    self._dsn = dsn
+
+  def get_connection(self):
+    """ Provides postgres connection. It can either return existing
+    working connection or establish new one.
+    Returns:
+      An instance of psycopg2 connection.
+    """
+    if not self._connection or self._connection.closed:
+      logger.info('Establishing new connection to Postgres server')
+      self._connection = psycopg2.connect(dsn=self._dsn)
+    return self._connection
+
+  def close(self):
+    """ Closes psycopg2 connection.
+    """
+    return self._connection.close()
+
+
+pg_connection_wrapper = None
+
+
+def connect_to_postgres(zk_client):
+  global pg_connection_wrapper
+  global_dsn_node = '/appscale/ua_server/postgres_dsn'
+  if zk_client.exists(global_dsn_node):
+    pg_dsn = zk_client.get(global_dsn_node)
+    logger.info('Using PostgreSQL as a backend for UA Server')
+  else:
+    pg_dsn = None
+    logger.info('Using Cassandra as a backend for UA Server')
+  if pg_dsn:
+    pg_connection_wrapper = (
+        PostgresConnectionWrapper(dsn=pg_dsn[0])
+    )
+
+
+@retry_pg_connection
 @tornado_synchronous
 @gen.coroutine
-def put_entity_sync(datastore, table_name, user, schema, array):
+def put_entity_sync(datastore, table_name, user, schema, user_data):
   """ Puts data of specified user from backup to datastore.
 
   Args:
@@ -27,9 +109,28 @@ def put_entity_sync(datastore, table_name, user, schema, array):
     table_name: Table name in datastore.
     user: User name.
     schema: Table schema.
-    array: List of all user's fields.
+    user_data: List or dict (if postgres role is enabled) of all user's fields.
   """
-  result = yield datastore.put_entity(table_name, user, schema, array)
+  if pg_connection_wrapper:
+    pg_connection = pg_connection_wrapper.get_connection()
+    with pg_connection:
+      with pg_connection.cursor() as pg_cursor:
+        pg_cursor.execute(
+          'INSERT INTO "{table}"'
+          'VALUES ( '
+          '  %(email)s, %(pw)s, %(date_creation)s, %(date_change)s, '
+          '  %(date_last_login)s, %(applications)s, %(appdrop_rem_token)s, '
+          '  %(appdrop_rem_token_exp)s, %(visit_cnt)s, %(cookie)s, '
+          '  %(cookie_ip)s, %(cookie_exp)s, %(cksum)s, %(enabled)s, %(type)s, '
+          '  %(is_cloud_admin)s, %(capabilities)s '
+          ') '
+          'RETURNING date_last_login'
+          .format(table=table_name),
+          vars=user_data
+        )
+        result = pg_cursor.fetchone()
+    raise gen.Return(result)
+  result = yield datastore.put_entity(table_name, user, schema, user_data)
   raise gen.Return(result)
 
 def main():
@@ -44,6 +145,13 @@ def main():
 
   if args.verbose:
     logging.getLogger('appscale').setLevel(logging.DEBUG)
+
+  # Configure zookeeper and db access
+  zk_client = KazooClient(
+    hosts=','.join(appscale_info.get_zk_node_ips()),
+    connection_retry=ZK_PERSISTENT_RECONNECTS)
+  zk_client.start()
+  connect_to_postgres(zk_client)
 
   datastore_type = 'cassandra'
 
@@ -80,5 +188,31 @@ def main():
     reader = csv.DictReader(fin, delimiter=',')
     # Iterate through all users in file
     for row in reader:
-      array = [row[key] for key in USERS_SCHEMA]
-      put_entity_sync(db, USERS_TABLE, array[0], user_schema, array)
+      if pg_connection_wrapper:
+        if not row['applications']:
+          row['applications'] = None
+        else:
+          # delete square brackets added by csv module
+          apps = row['applications'][1:-1]
+          # csv module adds extra quotes each time
+          apps = apps.replace("'", "")
+          row['applications'] = '{' + apps + '}'
+        put_entity_sync(db, table_name, row['email'], user_schema, row)
+      else:
+        # Convert dates to timestamp
+        t = str(time.mktime(datetime.datetime.strptime(
+          row['date_creation'], '%Y-%m-%d %H:%M:%S').timetuple()))
+        row['date_creation'] = t
+        t = str(time.mktime(datetime.datetime.strptime(
+          row['date_change'], '%Y-%m-%d %H:%M:%S').timetuple()))
+        row['date_change'] = t
+        t = str(time.mktime(datetime.datetime.strptime(
+          row['date_last_login'], '%Y-%m-%d %H:%M:%S').timetuple()))
+        row['date_last_login'] = t
+
+        apps = row['applications'][1:-1]
+        apps = apps.replace("'", "").replace(', ', ':')
+        row['applications'] = apps
+
+        array = [row[key] for key in USERS_SCHEMA]
+        put_entity_sync(db, USERS_TABLE, array[0], user_schema, array)
